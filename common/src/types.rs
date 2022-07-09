4d49cbbb3fee28dd9f9b9b82c8c1a598a4948f03
Junha Yang(양준하) <40292261+junha1@users.noreply.github.com>
2022-07-09 17:57:21 +0900

    Setup basic interface (#1)
    * Setup basic interface
    
    * Update method names and doc-comments
    
    * Make state_transition optional
    
    * Fix typo of lists to list
    
    * Use SocketAddrV4 instead of Ipv4Addr
    
    * Update field name

---
+++ common/src/types.rs
@@ -0,0 +1,13 @@
+//! Various types widely used in the project.
+
+/// A cryptographic hash.
+pub type Hash256 = ();
+
+/// A cryptographic signature.
+pub type Signature = ();
+
+/// A public key.
+pub type PublicKey = ();
+
+/// A private key.
+pub type PrivateKey = ();
