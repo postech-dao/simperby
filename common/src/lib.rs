283c120df70ad82fc4e8c1b6252d0b2ec706ee3a
Junha Yang <junhayang1@gmail.com>
2022-07-18 03:31:45 +0900

    Move definition of Header to common

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,1 +1,24 @@
 pub mod crypto;
+
+use crypto::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+pub struct BlockHeader {
+    /// The author of this block.
+    pub author: PublicKey,
+    /// The signature of the previous block.
+    pub prev_block_finalization_proof: Vec<Signature>,
+    /// The hash of the previous block.
+    pub previous_hash: Hash256,
+    /// The height of this block.
+    pub height: u64,
+    /// The timestamp of this block.
+    pub timestamp: u64,
+    /// The Merkle root of transactions.
+    pub tx_merkle_root: Hash256,
+    /// The Merkle root of the state.
+    pub state_merkle_root: Hash256,
+    /// The hash of the set of validator & vote weight for the next block.
+    pub validator_set_hash: Hash256,
+}
--- consensus/src/lib.rs
+++ consensus/src/lib.rs
@@ -1,6 +1,7 @@
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use simperby_common::crypto::*;
+use simperby_common::*;
 use std::sync::Arc;
 use tokio::sync::mpsc;
 
@@ -33,28 +34,8 @@ pub trait KVStore {
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
-pub struct Header {
-    /// The author of this block.
-    pub author: PublicKey,
-    /// The signature of the previous block.
-    pub prev_block_finalization_proof: Vec<Signature>,
-    /// The hash of the previous block.
-    pub previous_hash: Hash256,
-    /// The height of this block.
-    pub height: u64,
-    /// The timestamp of this block.
-    pub timestamp: u64,
-    /// The Merkle root of transactions.
-    pub tx_merkle_root: Hash256,
-    /// The Merkle root of the state.
-    pub state_merkle_root: Hash256,
-    /// The hash of the set of validator & vote weight for the next block.
-    pub validator_set_hash: Hash256,
-}
-
-#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub struct Block<T> {
-    pub header: Header,
+    pub header: BlockHeader,
     pub transactions: Vec<T>,
 }
 
@@ -78,7 +59,7 @@ pub trait Consensus<E: BlockExecutor> {
     async fn progress(
         &mut self,
         block_to_propose: Option<Block<E::Transaction>>,
-        last_finalized_header: Header,
+        last_finalized_header: BlockHeader,
         network: Arc<dyn Network>,
         executor: E,
         store: Box<dyn KVStore>,
