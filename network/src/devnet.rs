c77ebed06aa855cd40f9a540ffead39a1ffa5832
lemonholic <lemonwater@postech.ac.kr>
2022-07-20 18:20:52 +0900

    Remove trait comments from trait implementation

--- network/src/devnet.rs
+++ network/src/devnet.rs
@@ -11,7 +11,6 @@ pub struct DevNet {}
 
 #[async_trait]
 impl AuthorizedNetwork for DevNet {
-    /// Joins the network with an authorized identity.
     async fn new(
         _public_key: PublicKey,
         _private_key: PrivateKey,
@@ -24,15 +23,12 @@ impl AuthorizedNetwork for DevNet {
     {
         unimplemented!("not implemented");
     }
-    /// Broadcasts a message to the network, after signed by the key given to this instance.
     async fn broadcast(&self, _message: &[u8]) -> Result<(), String> {
         unimplemented!("not implemented");
     }
-    /// Creates a receiver for every message broadcasted to the network, except the one sent by this instance.
     async fn create_recv_queue(&self) -> Result<mpsc::Receiver<Vec<u8>>, ()> {
         unimplemented!("not implemented");
     }
-    /// Provides the estimated list of live nodes that are eligible and identified by their public keys.
     async fn get_live_list(&self) -> Result<Vec<PublicKey>, ()> {
         unimplemented!("not implemented");
     }
