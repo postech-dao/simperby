04f0e4fae2a987eefd97e203ebf0949d144412ab
lemonholic <lemonwater@postech.ac.kr>
2022-07-29 16:35:55 +0900

    docs: format `use` and `mod` statements

--- network/src/propagation_network/behaviour.rs
+++ network/src/propagation_network/behaviour.rs
@@ -9,17 +9,17 @@ use libp2p::{
 #[behaviour(out_event = "Event")]
 pub struct Behaviour {
     /// A network behaviour that identifies connected peers.
-    /// 
+    ///
     /// Information of the identified peer contains its public key, listen addresses, etc.
     identify: Identify,
     /// A network behaviour that implement Kademlia Distributed Hash Table (Kademlia DHT).
-    /// 
+    ///
     /// Storing and retrieving items in/from the DHT do not occur in this crate.
     /// Instead, kademlia continuously discovers k closest peers
     /// to maintain k connections with its neighbors.
     kademlia: Kademlia<MemoryStore>,
     /// A network behaviour that implements PubSub message passing protocol.
-    /// 
+    ///
     /// It tries to propagate a message to all peers that it has connections with,
     /// thus flooding the network with messages.
     floodsub: Floodsub,
