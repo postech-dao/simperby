3751ad04b4cfea2f59cee3223a9e2a7a7e0535f7
Changhun Oh <milchstra3e@gmail.com>
2022-07-30 22:29:00 +0900

    feat: implement rocksdb_kv functions
    include rocksdb crate
    declare new field db in RocksDB struct
    implement function new / open / insert_or_update / remove / get

--- kv-storage/src/rocksdb_kv/mod.rs
+++ kv-storage/src/rocksdb_kv/mod.rs
@@ -1,22 +1,23 @@
 use super::*;
+use rocksdb::DB;
 
 pub struct RocksDB {
-    // TODO
+    db: DB,
 }
 
 #[async_trait]
 impl KVStore for RocksDB {
-    async fn new(_path: &str) -> Result<Self, ()>
+    async fn new(path: &str) -> Result<Self, ()>
     where
         Self: Sized,
     {
-        unimplemented!("not implemented");
+        Ok(RocksDB { db: DB::open_default(path).unwrap() })
     }
-    async fn open(_path: &str) -> Result<Self, ()>
+    async fn open(path: &str) -> Result<Self, ()>
     where
         Self: Sized,
     {
-        unimplemented!("not implemented");   
+        Ok(RocksDB { db: DB::open_default(path).unwrap() })
     }
     async fn commit_checkpoint(&mut self) -> Result<(), ()> {
         unimplemented!("not implemented");
@@ -24,13 +25,25 @@ impl KVStore for RocksDB {
     async fn revert_to_latest_checkpoint(&mut self) -> Result<(), ()> {
         unimplemented!("not implemented");
     }
-    async fn insert_or_update(&mut self, _key: Hash256, _value: &[u8]) -> Result<(), ()> {
-        unimplemented!("not implemented");
+    async fn insert_or_update(&mut self, key: Hash256, value: &[u8]) -> Result<(), ()> {
+        let result = self.db.put(key.dummy, value);
+        match result {
+            Ok(_) => Ok(()),
+            Err(_) => Err(()),
+        }
     }
-    async fn remove(&mut self, _key: Hash256) -> Result<(), ()> {
-        unimplemented!("not implemented");
+    async fn remove(&mut self, key: Hash256) -> Result<(), ()> {
+        let result = self.db.delete(key.dummy);
+        match result {
+            Ok(_) => Ok(()),
+            Err(_) => Err(()),
+        }
     }
-    async fn get(&self, _key: Hash256) -> Result<Option<Vec<u8>>, ()> {
-        unimplemented!("not implemented");
+    async fn get(&self, key: Hash256) -> Result<Option<Vec<u8>>, ()> {
+        let result = self.db.get(key.dummy);
+        match result {
+            Ok(v) => Ok(v),
+            Err(_) => Err(()),
+        }
     }
 }
