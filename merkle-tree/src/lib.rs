75274ee67692f2e62b8507296708bd89138532ea
TomTaehoonKim <taehoontomkim@gmail.com>
2022-08-06 20:09:00 +0900

    feat: Setup merkle tree interface

--- merkle-tree/src/lib.rs
+++ merkle-tree/src/lib.rs
@@ -1,1 +1,45 @@
+use rocksdb::DB;
+use simperby_common::crypto::*;
 
+pub struct Node {
+    pub key: Hash256,
+    pub value: Option<Vec<u8>>,
+    pub left: Option<Box<Node>>,
+    pub right: Option<Box<Node>>,
+}
+
+pub struct MerkleTree {
+    pub root: Box<Node>,
+}
+
+impl MerkleTree {
+    // Creates a tree with an existing root node from database
+    pub fn new(_root: Option<Hash256>, _db: Option<DB>) -> Self {
+        unimplemented!();
+    }
+
+    // Gets the value for key stored in the tree
+    pub fn get(&self, _key: Hash256) -> &[u8] {
+        unimplemented!();
+    }
+
+    // Updates value with key in the tree
+    pub fn update(&self, _key: Hash256, _value: &[u8]) {
+        unimplemented!();
+    }
+
+    // Inserts key and value into the tree
+    pub fn insert(&self, _key: Hash256, _value: &[u8]) {
+        unimplemented!();
+    }
+
+    // Deletes any existing value for key from the tree
+    pub fn delete(&self, _key: Hash256) {
+        unimplemented!();
+    }
+
+    // Calculates the root hash of the given tree
+    pub fn hash_root(&self) -> Hash256 {
+        unimplemented!();
+    }
+}
