f52a631326bf055b91426630b594e706a43a27e5
Junha Yang <junhayang1@gmail.com>
2022-08-08 12:06:53 +0900

    feat: provide error type for KVStorage

--- kv-storage/src/lib.rs
+++ kv-storage/src/lib.rs
@@ -1,5 +1,14 @@
 use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
 use simperby_common::crypto::*;
+use thiserror::Error;
+
+#[derive(Error, Debug, Serialize, Deserialize, Clone)]
+pub enum Error {
+    /// An unknown error
+    #[error("unknown: {0}")]
+    Unknown(String),
+}
 
 /// TODO: Provide error types.
 ///
@@ -7,21 +16,21 @@ use simperby_common::crypto::*;
 #[async_trait]
 pub trait KVStorage {
     /// Creates an empty store with the path to newly create.
-    async fn new(path: &str) -> Result<Self, ()>
+    async fn new(path: &str) -> Result<Self, Error>
     where
         Self: Sized;
     /// Open an existing store with the path given.
-    async fn open(path: &str) -> Result<Self, ()>
+    async fn open(path: &str) -> Result<Self, Error>
     where
         Self: Sized;
     /// Records the current state to the persistent storage.
-    async fn commit_checkpoint(&mut self) -> Result<(), ()>;
+    async fn commit_checkpoint(&mut self) -> Result<(), Error>;
     /// Reverts all the changes made since the last checkpoint.
-    async fn revert_to_latest_checkpoint(&mut self) -> Result<(), ()>;
+    async fn revert_to_latest_checkpoint(&mut self) -> Result<(), Error>;
     /// Inserts a key-value pair into the store. If exists, it will be overwritten.
-    async fn insert_or_update(&mut self, key: Hash256, value: &[u8]) -> Result<(), ()>;
+    async fn insert_or_update(&mut self, key: Hash256, value: &[u8]) -> Result<(), Error>;
     /// Removes a key-value pair from the store. If not exists, it will fail.
-    async fn remove(&mut self, key: Hash256) -> Result<(), ()>;
+    async fn remove(&mut self, key: Hash256) -> Result<(), Error>;
     /// Retrieves the value associated with the key. If not exists, it will return `None`.
-    async fn get(&self, key: Hash256) -> Result<Option<Vec<u8>>, ()>;
+    async fn get(&self, key: Hash256) -> Result<Option<Vec<u8>>, Error>;
 }
