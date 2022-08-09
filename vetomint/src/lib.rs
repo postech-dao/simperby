e593eac3bd48aff74259abbdada829909856a7b3
Junha Yang <junhayang1@gmail.com>
2022-08-09 18:03:01 +0900

    feat: setup basics of vetomint
    1. It involves some interface changes.
    2. The reason that the state machine keeps the event queue is for
    handling asynchronous `getValue()`.
    3. Still many logics are unimplemented

--- vetomint/src/lib.rs
+++ vetomint/src/lib.rs
@@ -1,17 +1,23 @@
 mod progress;
 
 use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
 
 /// An index of the validator, which is for a single height. (Mapping from the actual public key to the index may differ for different heights.)
 pub type ValidatorIndex = usize;
 /// An identifier of the block, which is uniquely mapped to a block. Like `ValidatorIndex`, it is for a single height. (Mapping from the actual block to the index may differ for different heights.)
 pub type BlockIdentifier = usize;
+/// A round.
+pub type Round = usize;
+/// A voting power.
+pub type VotingPower = u64;
 /// A UNIX timestamp measured in milliseconds.
 pub type Timestamp = i64;
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
 pub struct ConsensusParams {
     pub timeout_ms: u64,
+    pub repeat_round_for_first_leader: usize,
 }
 
 /// An event that (potentially) triggers a state transition of `StateMachine`.
@@ -22,11 +28,13 @@ pub struct ConsensusParams {
 /// the lower layer will keep the mapping from the actual data to the indices.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum ConsensusEvent {
+    /// Signals to start the process
+    Start { time: Timestamp },
     /// Informs that the node has received a block proposal.
-    BlockProposal {
+    BlockProposalReceived {
         proposal: BlockIdentifier,
         proposer: ValidatorIndex,
-        round: usize,
+        round: Round,
         time: Timestamp,
     },
     /// Informs that the node is in favor of or against a proposal.
@@ -36,62 +44,82 @@ pub enum ConsensusEvent {
         favor: bool,
         time: Timestamp,
     },
-    /// Informs that `CreateAndBroadcastProposal` has been completed.
-    BlockProposalBroadcasted {
+    /// Informs that `CreateProposal` has been completed.
+    BlockProposalCreated {
         proposal: BlockIdentifier,
-        round: usize,
+        round: Round,
         time: Timestamp,
     },
     /// Informs that the node has received a block prevote.
     Prevote {
         proposal: BlockIdentifier,
         signer: ValidatorIndex,
-        round: usize,
+        round: Round,
         time: Timestamp,
     },
     /// Informs that the node has received a block precommit.
     Precommit {
         proposal: BlockIdentifier,
         signer: ValidatorIndex,
-        round: usize,
+        round: Round,
         time: Timestamp,
     },
     /// Informs that the node has received a nil prevote.
     NilPrevote {
         signer: ValidatorIndex,
-        round: usize,
+        round: Round,
         time: Timestamp,
     },
     /// Informs that the node has received a nil precommit.
     NilPrecommit {
         signer: ValidatorIndex,
-        round: usize,
+        round: Round,
         time: Timestamp,
     },
     /// Informs that time has passed.
     Timer { time: Timestamp },
 }
 
+impl ConsensusEvent {
+    fn time(&self) -> Timestamp {
+        match self {
+            ConsensusEvent::Start { time, .. } => *time,
+            ConsensusEvent::BlockProposalReceived { time, .. } => *time,
+            ConsensusEvent::ProposalFavor { time, .. } => *time,
+            ConsensusEvent::BlockProposalCreated { time, .. } => *time,
+            ConsensusEvent::Prevote { time, .. } => *time,
+            ConsensusEvent::Precommit { time, .. } => *time,
+            ConsensusEvent::NilPrevote { time, .. } => *time,
+            ConsensusEvent::NilPrecommit { time, .. } => *time,
+            ConsensusEvent::Timer { time, .. } => *time,
+        }
+    }
+}
+
 /// A response that the consensus might emit for a given event, which must be properly handled by the lower layer.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum ConsensusResponse {
     /// Creation of the actual proposal is not the role of the consensus; the lower layer will take care of it.
-    CreateAndBroadcastProposal {
-        round: usize,
+    CreateProposal {
+        round: Round,
+    },
+    BroadcastProposal {
+        proposal: BlockIdentifier,
+        round: Round,
     },
     BroadcastPrevote {
         proposal: BlockIdentifier,
-        round: usize,
+        round: Round,
     },
     BroadcastPrecommit {
         proposal: BlockIdentifier,
-        round: usize,
+        round: Round,
     },
     BroadcastNilPrevote {
-        round: usize,
+        round: Round,
     },
     BroadcastNilPrecommit {
-        round: usize,
+        round: Round,
     },
     FinalizeBlock {
         proposal: BlockIdentifier,
@@ -108,7 +136,7 @@ pub struct HeightInfo {
     /// The list of voting powers sorted by the leader order.
     ///
     /// Important note: `ValidatorIndex` is used to index this list.
-    pub validators: Vec<u64>,
+    pub validators: Vec<VotingPower>,
 
     /// The index of this node
     pub this_node_index: ValidatorIndex,
@@ -120,18 +148,72 @@ pub struct HeightInfo {
     pub consensus_params: ConsensusParams,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum ConsensusStep {
+    Initial,
+    Propose,
+    Prevote,
+    Precommit,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+struct Votes {
+    prevotes_total: VotingPower,
+    prevotes_favor: BTreeMap<BlockIdentifier, VotingPower>,
+    /// If on-5f-prevotes has ever been triggered?
+    triggered_5f_prevote: bool,
+    // TODO: add precommits
+}
+
 /// The state of the consensus during a single height.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ConsensusState {
-    round: usize,
-    // TODO: One typical implementation would have some kind of a verbose `enum` of the state variants.
+    step: ConsensusStep,
+    round: Round,
+    locked_value: Option<BlockIdentifier>,
+    locked_round: Option<Round>,
+    valid_value: Option<BlockIdentifier>,
+    valid_round: Option<Round>,
+    timeout_propose: Option<Timestamp>,
+
+    votes: BTreeMap<Round, Votes>,
+    waiting_for_proposal_creation: bool,
 }
 
 impl ConsensusState {
     /// Prepares the initial state of the consensus.
     pub fn new(_height_info: HeightInfo) -> Self {
-        ConsensusState { round: 0 }
+        ConsensusState {
+            step: ConsensusStep::Initial,
+            round: 0,
+            locked_value: None,
+            locked_round: None,
+            valid_value: None,
+            valid_round: None,
+            timeout_propose: None,
+            votes: Default::default(),
+            waiting_for_proposal_creation: false,
+        }
+    }
+
+    /// Makes a progress of the state machine with the given event.
+    ///
+    /// It returns `None` if the state machine is not ready to process the event.
+    /// It returns `Some(Vec![])` if the state machine processed the event but did not emit any response.
+    pub fn progress(
+        &mut self,
+        height_info: &HeightInfo,
+        event: ConsensusEvent,
+    ) -> Option<Vec<ConsensusResponse>> {
+        progress::progress(height_info, self, event)
     }
 }
 
-pub use progress::progress;
+pub fn decide_proposer(round: usize, height_info: &HeightInfo) -> ValidatorIndex {
+    if round < height_info.consensus_params.repeat_round_for_first_leader {
+        0
+    } else {
+        (round - height_info.consensus_params.repeat_round_for_first_leader + 1)
+            % height_info.validators.len()
+    }
+}
--- vetomint/src/progress.rs
+++ vetomint/src/progress.rs
@@ -1,9 +1,158 @@
 use super::*;
 
-pub fn progress(
-    _height_info: &HeightInfo,
-    _state: &mut ConsensusState,
-    _event: ConsensusEvent,
+/// TODO: we have to implement the following missing logics
+/// - on-proposal
+/// - on-4f-favor-prevote-propose-step
+/// - on-4f-favor-prevote-prevote-step
+/// - on-4f-nil-prevote
+/// - on-5f-precommit
+/// - on-4f-favor-precommit
+/// - OnTimeoutPrecommit
+pub(super) fn progress(
+    height_info: &HeightInfo,
+    state: &mut ConsensusState,
+    event: ConsensusEvent,
+) -> Option<Vec<ConsensusResponse>> {
+    let result = if state.waiting_for_proposal_creation {
+        if let ConsensusEvent::BlockProposalCreated {
+            proposal, round, ..
+        } = event
+        {
+            if state.round != round {
+                return None;
+            }
+            state.waiting_for_proposal_creation = false;
+            return Some(vec![ConsensusResponse::BroadcastProposal {
+                proposal,
+                round: state.round,
+            }]);
+        } else {
+            // Nothing to do; this state is waiting for a `BlockProposalCreated`.
+            return None;
+        }
+    } else {
+        match event {
+            ConsensusEvent::Start { time } => match start_round(height_info, state, 0, time) {
+                StartRoundResponse::Normal(r) => r,
+                StartRoundResponse::Pending { .. } => {
+                    state.waiting_for_proposal_creation = true;
+                    Vec::new()
+                }
+            },
+            ConsensusEvent::BlockProposalCreated { .. } => return None,
+            // Time-trigger events are handled later
+            ConsensusEvent::Timer { .. } => Vec::new(),
+            ConsensusEvent::Prevote {
+                proposal,
+                signer,
+                round,
+                ..
+            } => {
+                let total_voting_power = height_info.validators.iter().sum::<VotingPower>();
+                if round != state.round {
+                    return None;
+                }
+                let voting_power = height_info.validators[signer as usize];
+                state.votes.insert(round, {
+                    let mut votes = state
+                        .votes
+                        .get(&round)
+                        .unwrap_or(&Default::default())
+                        .clone();
+                    votes.prevotes_total += voting_power;
+                    votes.prevotes_favor.insert(
+                        proposal,
+                        votes.prevotes_favor.get(&proposal).unwrap_or(&0) + voting_power,
+                    );
+                    votes
+                });
+                if state.votes[&round].prevotes_total * 6 > total_voting_power * 5
+                    && !state.votes[&round].triggered_5f_prevote
+                    && state.step == ConsensusStep::Prevote
+                {
+                    on_5f_prevote(height_info, state, round)
+                } else {
+                    Vec::new()
+                }
+            }
+            _ => unimplemented!(),
+        }
+    };
+
+    if !result.is_empty() {
+        Some(result)
+    // Handle timeout
+    } else {
+        let time = event.time();
+        let mut responses = Vec::new();
+        if let Some(timeout_propose) = state.timeout_propose {
+            if time >= timeout_propose {
+                responses.append(&mut on_timeout_propose(height_info, state, state.round));
+            }
+        }
+        Some(responses)
+    }
+}
+
+enum StartRoundResponse {
+    Normal(Vec<ConsensusResponse>),
+    /// Emits a `CreateProposal`.
+    Pending,
+}
+
+fn start_round(
+    height_info: &HeightInfo,
+    state: &mut ConsensusState,
+    round: usize,
+    time: Timestamp,
+) -> StartRoundResponse {
+    state.round = round;
+    state.step = ConsensusStep::Propose;
+    let proposer = decide_proposer(round, height_info);
+    if proposer == height_info.this_node_index {
+        if let Some(valid_value) = state.valid_value {
+            StartRoundResponse::Normal(vec![ConsensusResponse::BroadcastProposal {
+                proposal: valid_value,
+                round,
+            }])
+        } else {
+            StartRoundResponse::Pending
+        }
+    } else {
+        state.timeout_propose = Some(time + height_info.consensus_params.timeout_ms as i64);
+        StartRoundResponse::Normal(Vec::new())
+    }
+}
+
+fn on_5f_prevote(
+    height_info: &HeightInfo,
+    state: &mut ConsensusState,
+    round: Round,
 ) -> Vec<ConsensusResponse> {
-    unimplemented!()
+    state.votes.get_mut(&round).unwrap().triggered_5f_prevote = true;
+    let total_voting_power = height_info.validators.iter().sum::<VotingPower>();
+    state.step = ConsensusStep::Precommit;
+    for (proposal, prevotes_favor) in &state.votes[&round].prevotes_favor {
+        if prevotes_favor * 3 > total_voting_power * 2 {
+            return vec![ConsensusResponse::BroadcastPrecommit {
+                proposal: *proposal,
+                round: state.round,
+            }];
+        }
+    }
+    vec![ConsensusResponse::BroadcastNilPrecommit { round: state.round }]
+}
+
+fn on_timeout_propose(
+    _height_info: &HeightInfo,
+    state: &mut ConsensusState,
+    round: usize,
+) -> Vec<ConsensusResponse> {
+    if state.round == round && state.step == ConsensusStep::Propose {
+        state.step = ConsensusStep::Prevote;
+        state.timeout_propose = None;
+        vec![ConsensusResponse::BroadcastNilPrevote { round }]
+    } else {
+        Vec::new()
+    }
 }
