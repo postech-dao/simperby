0a89295d364640d3b9ed8d072070ce7371a93eae
Junha Yang <junhayang1@gmail.com>
2022-08-25 00:57:49 +0900

    feat: update NodeState interface

---
+++ simperby/src/node/network.rs
@@ -0,0 +1,12 @@
+use super::*;
+
+pub(super) enum NetworkOperation {
+    // TODO
+}
+
+pub(super) async fn run_network_task(
+    _network: Box<dyn AuthorizedNetwork>,
+    _node_state: Arc<RwLock<NodeState>>,
+) {
+    unimplemented!()
+}
