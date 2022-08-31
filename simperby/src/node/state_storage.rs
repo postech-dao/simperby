abe5eac5055f62e3f7d8409d8d2fe855c34baa07
Junha Yang <junhayang1@gmail.com>
2022-08-31 23:08:00 +0900

    feat: implement StateStorage

--- simperby/src/node/state_storage.rs
+++ simperby/src/node/state_storage.rs
@@ -1,7 +1,3 @@
-#![allow(unused_mut)]
-#![allow(dead_code)]
-#![allow(unused_variables)]
-
 use crate::*;
 use thiserror::Error;
 
@@ -16,13 +12,107 @@ pub enum StateStorageError {
     StorageIntegrityError(String),
 }
 
-impl From<Error> for SimperbyError {
-    fn from(e: Error) -> Self {
-        unimplemented!()
+type Error = StateStorageError;
+
+impl From<simperby_kv_storage::Error> for Error {
+    fn from(e: simperby_kv_storage::Error) -> Self {
+        Error::StorageError(e)
     }
 }
 
-type Error = StateStorageError;
+impl From<Error> for SimperbyError {
+    fn from(e: Error) -> Self {
+        match e {
+            Error::InvalidOperation(e) => SimperbyError::InvalidOperation(e),
+            Error::StorageError(e) => SimperbyError::StorageError(e),
+            Error::StorageIntegrityError(e) => SimperbyError::StorageIntegrityError(e),
+        }
+    }
+}
+
+/// Reads an essential (existence-guaranteed) item from the storage.
+async fn get_json_from_storage<T>(storage: &dyn KVStorage, key: Hash256) -> Result<T, Error>
+where
+    T: serde::de::DeserializeOwned,
+{
+    let value = match storage.get(key.clone()).await {
+        Ok(x) => x,
+        Err(simperby_kv_storage::Error::NotFound) => {
+            return Err(Error::StorageIntegrityError(format!(
+                "missing essential item in the state stroage: {}",
+                key
+            )))
+        }
+        Err(e) => return Err(e.into()),
+    };
+    serde_json::from_slice(&value).map_err(|e| Error::StorageIntegrityError(e.to_string()))
+}
+
+async fn get_hash_from_storage(storage: &dyn KVStorage, key: Hash256) -> Result<Hash256, Error> {
+    let value = match storage.get(key.clone()).await {
+        Ok(x) => x,
+        Err(simperby_kv_storage::Error::NotFound) => {
+            return Err(Error::StorageIntegrityError(format!(
+                "missing essential item in the state stroage: {}",
+                key
+            )))
+        }
+        Err(e) => return Err(e.into()),
+    };
+    let msg = if value.len() > 1024 {
+        format!(
+            "item hash is corrupted: (too long; emited; {})",
+            value.len()
+        )
+    } else {
+        format!("item hash is corrupted: {:?}", value)
+    };
+    let data: [u8; 32] = TryInto::try_into(value).map_err(|_| Error::StorageIntegrityError(msg))?;
+    Ok(Hash256::from_array(data))
+}
+
+async fn insert_or_update_json_to_storage<T>(
+    storage: &mut dyn KVStorage,
+    key: Hash256,
+    value: &T,
+) -> Result<(), Error>
+where
+    T: serde::Serialize,
+{
+    let value = serde_json::to_vec(value).unwrap();
+    let hash = Hash256::hash(&value);
+    storage.insert_or_update(key.clone(), &value).await?;
+    storage
+        .insert_or_update(create_hash_key(key), hash.as_ref())
+        .await?;
+    Ok(())
+}
+
+async fn insert_or_update_bytes_to_storage(
+    storage: &mut dyn KVStorage,
+    key: Hash256,
+    data: &[u8],
+) -> Result<Hash256, Error> {
+    let hash = Hash256::hash(data);
+    storage.insert_or_update(key.clone(), data).await?;
+    storage
+        .insert_or_update(create_hash_key(key), hash.as_ref())
+        .await?;
+    Ok(hash)
+}
+
+/// The state storage keeps data hashes for every entry.
+fn create_hash_key(hash: Hash256) -> Hash256 {
+    hash.aggregate(&Hash256::hash("hash-key"))
+}
+
+fn create_delegation_state_key() -> Hash256 {
+    Hash256::hash("delegation-state")
+}
+
+fn create_state_root() -> Hash256 {
+    Hash256::hash("state-root")
+}
 
 pub(crate) struct StateStorage {
     pub storage: Box<dyn KVStorage>,
@@ -32,46 +122,90 @@ impl StateStorage {
     /// Assuming an empty storage, creates the genesis state.
     pub async fn genesis(
         mut storage: Box<dyn KVStorage>,
-        genesis_info: GenesisInfo,
+        _genesis_info: GenesisInfo,
     ) -> Result<Self, Error> {
-        unimplemented!()
+        insert_or_update_json_to_storage(
+            storage.as_mut(),
+            create_delegation_state_key(),
+            &DelegationState {
+                original_validator_set: Default::default(),
+            },
+        )
+        .await?;
+        storage
+            .insert_or_update(create_state_root(), Hash256::zero().as_ref())
+            .await?;
+        Ok(Self { storage })
     }
 
     /// Assuming an existing state, check the basic validity of the state.
     pub async fn new(storage: Box<dyn KVStorage>) -> Result<Self, Error> {
-        unimplemented!()
+        get_json_from_storage::<DelegationState>(storage.as_ref(), create_delegation_state_key())
+            .await?;
+        get_hash_from_storage(storage.as_ref(), create_state_root()).await?;
+        Ok(Self { storage })
     }
 
     pub async fn delegation_state_hash(&self) -> Result<Hash256, Error> {
-        unimplemented!()
+        get_hash_from_storage(
+            self.storage.as_ref(),
+            create_hash_key(create_delegation_state_key()),
+        )
+        .await
     }
 
     pub async fn state_root(&self) -> Result<Hash256, Error> {
-        unimplemented!()
+        get_hash_from_storage(self.storage.as_ref(), create_state_root()).await
     }
 
     /// Atomically inserts data, hash of it, and updates the state root.
     pub async fn insert_or_update_data(&mut self, key: Hash256, data: &[u8]) -> Result<(), Error> {
-        unimplemented!()
+        let hash = insert_or_update_bytes_to_storage(self.storage.as_mut(), key, data).await?;
+        // TODO: introduce state Merkle tree.
+        //
+        // Currently the state root is just an arbitrary hash of the entire operation of the state history
+        // which's deterministically decided, so that it can be used for the consensus.
+        let state_root = self.state_root().await?;
+        let new_state_root = state_root.aggregate(&hash);
+        self.storage
+            .insert_or_update(create_state_root(), new_state_root.as_ref())
+            .await?;
+        Ok(())
     }
 
     pub async fn remove_data(&mut self, key: Hash256) -> Result<(), Error> {
-        unimplemented!()
+        let state_root = self.state_root().await?;
+        let new_state_root = state_root.aggregate(&Hash256::hash("remove"));
+        self.storage.remove(key.clone()).await?;
+        self.storage.remove(create_hash_key(key)).await?;
+        self.storage
+            .insert_or_update(create_state_root(), new_state_root.as_ref())
+            .await?;
+        Ok(())
     }
 
     pub async fn get_delegation_state(&self) -> Result<DelegationState, Error> {
-        unimplemented!()
+        get_json_from_storage::<DelegationState>(
+            self.storage.as_ref(),
+            create_delegation_state_key(),
+        )
+        .await
     }
 
     pub async fn update_delegation_state(&mut self, state: DelegationState) -> Result<(), Error> {
-        unimplemented!()
+        insert_or_update_json_to_storage(
+            self.storage.as_mut(),
+            create_delegation_state_key(),
+            &state,
+        )
+        .await
     }
 
     pub async fn get_data(&self, key: Hash256) -> Result<Vec<u8>, Error> {
-        unimplemented!()
+        self.storage.get(key).await.map_err(|e| e.into())
     }
 
     pub async fn get_data_hash(&self, key: Hash256) -> Result<Hash256, Error> {
-        unimplemented!()
+        get_hash_from_storage(self.storage.as_ref(), key).await
     }
 }
