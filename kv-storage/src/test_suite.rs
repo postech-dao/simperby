4ca6725b7c140b28718294f12e159d2852778c23
TomTaehoonKim <taehoontomkim@gmail.com>
2022-09-05 18:22:50 +0900

    test: add common test scenarios for `KVStorage`

--- kv-storage/src/test_suite.rs
+++ kv-storage/src/test_suite.rs
@@ -3,6 +3,19 @@
 use super::*;
 
 #[allow(dead_code)]
+async fn remove_non_existing_key(storage: &mut dyn KVStorage) {
+    assert_eq!(
+        storage.remove(Hash256::hash("1")).await,
+        Err(Error::NotFound)
+    );
+}
+
+#[allow(dead_code)]
+async fn get_non_existing_key(storage: &mut dyn KVStorage) {
+    assert_eq!(storage.get(Hash256::hash("1")).await, Err(Error::NotFound));
+}
+
+#[allow(dead_code)]
 async fn single_crud_1(storage: &mut dyn KVStorage) {
     storage
         .insert_or_update(Hash256::hash("1"), b"1")
@@ -19,4 +32,51 @@ async fn single_crud_1(storage: &mut dyn KVStorage) {
         storage.get(Hash256::hash("1")).await,
         Err(crate::Error::NotFound)
     );
+}
+
+#[allow(dead_code)]
+async fn single_crud_2(storage: &mut dyn KVStorage) {
+    storage
+        .insert_or_update(Hash256::hash("1"), b"1")
+        .await
+        .unwrap();
+    assert_eq!(storage.get(Hash256::hash("1")).await.unwrap(), b"1");
+    storage
+        .insert_or_update(Hash256::hash("1"), b"2")
+        .await
+        .unwrap();
+    assert_eq!(storage.get(Hash256::hash("1")).await.unwrap(), b"2");
+    storage.remove(Hash256::hash("1")).await.unwrap();
+    assert_eq!(
+        storage.remove(Hash256::hash("1")).await,
+        Err(Error::NotFound)
+    );
+}
+
+#[allow(dead_code)]
+async fn multiple_crud_1(storage: &mut dyn KVStorage) {
+    storage
+        .insert_or_update(Hash256::hash("1"), b"1")
+        .await
+        .unwrap();
+    storage
+        .insert_or_update(Hash256::hash("2"), b"2")
+        .await
+        .unwrap();
+    assert_eq!(storage.get(Hash256::hash("1")).await.unwrap(), b"1");
+    assert_eq!(storage.get(Hash256::hash("2")).await.unwrap(), b"2");
+    storage
+        .insert_or_update(Hash256::hash("1"), b"2")
+        .await
+        .unwrap();
+    storage
+        .insert_or_update(Hash256::hash("2"), b"1")
+        .await
+        .unwrap();
+    assert_eq!(storage.get(Hash256::hash("1")).await.unwrap(), b"2");
+    assert_eq!(storage.get(Hash256::hash("2")).await.unwrap(), b"1");
+    storage.remove(Hash256::hash("1")).await.unwrap();
+    storage.remove(Hash256::hash("2")).await.unwrap();
+    assert_eq!(storage.get(Hash256::hash("1")).await, Err(Error::NotFound));
+    assert_eq!(storage.get(Hash256::hash("2")).await, Err(Error::NotFound));
 }
