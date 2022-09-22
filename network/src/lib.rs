a84b544c0a2ff7d218a425483dcdacc92d9bd31e
Junha Yang <junhayang1@gmail.com>
2022-09-22 19:18:28 +0900

    feat: introduce new network interface

--- network/src/lib.rs
+++ network/src/lib.rs
@@ -3,61 +3,133 @@ pub mod propagation_network;
 
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
-use simperby_common::crypto::*;
-use std::net::SocketAddrV4;
-use tokio::sync::broadcast;
+use simperby_common::{crypto::*, BlockHeight};
+use std::{net::SocketAddrV4, sync::Arc};
+use thiserror::Error;
+use tokio::sync::{mpsc, RwLock};
 
-pub type BroadcastToken = u64;
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum Error {
+    #[error("unknown error: {0}")]
+    Unknown(String),
+}
+
+pub struct Peer {
+    pub public_key: PublicKey,
+    pub address: SocketAddrV4,
+    /// An arbitrary string that the peer has set for itself.
+    /// This is usually used for indicating ports for the other services
+    /// that ths peer is running (e.g., Git, RPC, Message, ...)
+    pub message: String,
+}
+
+#[async_trait]
+pub trait PeerDiscovery {
+    /// Remains online on the network indefinitely,
+    /// responding to discovery requests from other nodes,
+    /// updating `known_peers`.
+    async fn serve(
+        network_config: &NetworkConfig,
+        known_peers: Arc<RwLock<Vec<Peer>>>,
+    ) -> Result<(), Error>;
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct BroadcastStatus {
-    relayed_nodes: Vec<PublicKey>,
+pub struct NetworkConfig {
+    /// The set of the members of the network.
+    pub members: Vec<PublicKey>,
+    /// The public key of this node.
+    pub public_key: PublicKey,
+    /// The private key of this node.
+    pub private_key: PrivateKey,
 }
 
-/// TODO: Provide error types.
-///
-/// Note: This trait is quite subject to change.
+/// The p2p gossip network.
 #[async_trait]
-pub trait AuthorizedNetwork: Send + Sync {
-    /// Joins the network with an authorized identity.
-    async fn new(
-        public_key: PublicKey,
-        private_key: PrivateKey,
-        known_peers: Vec<PublicKey>,
-        bootstrap_points: Vec<SocketAddrV4>,
-        network_id: String,
-    ) -> Result<Self, String>
-    where
-        Self: Sized;
-    /// Broadcasts a message to the network, after signed by the key given to this instance.
-    async fn broadcast(&self, message: &[u8]) -> Result<BroadcastToken, String>;
-    /// Stops a currently broadcasting message.
-    async fn stop_broadcast(&self, token: BroadcastToken) -> Result<(), String>;
-    /// Gets the current status of a broadcasting message.
-    async fn get_broadcast_status(&self, token: BroadcastToken) -> Result<BroadcastStatus, String>;
-    /// Creates a receiver for every message broadcasted to the network, except the one sent by this instance.
-    async fn create_recv_queue(&self) -> Result<broadcast::Receiver<Vec<u8>>, ()>;
-    /// Provides the estimated list of live nodes that are eligible and identified by their public keys.
-    async fn get_live_list(&self) -> Result<Vec<PublicKey>, ()>;
-}
-
-/// TODO: Provide error types.
-///
-/// Note: This trait is quite subject to change.
-#[async_trait]
-pub trait UnauthorizedNetwork: Send + Sync {
-    /// Joins the network with an authorized identity.
-    async fn new(
-        bootstrap_points: Vec<SocketAddrV4>,
-        known_peers: Vec<PublicKey>,
-        network_id: String,
-    ) -> Result<Self, String>
-    where
-        Self: Sized;
+pub trait P2PNetwork {
     /// Broadcasts a message to the network.
-    async fn broadcast(&self, message: &[u8]) -> Result<(), String>;
-    /// Creates a receiver for every message broadcasted to the network, except the one sent by this instance.
-    async fn create_recv_queue(&self) -> Result<broadcast::Receiver<Vec<u8>>, ()>;
-    /// Provides the estimated list of live nodes identified by their IP addresses
-    async fn get_live_list(&self) -> Result<Vec<std::net::SocketAddrV4>, ()>;
+    async fn broadcast(
+        config: &NetworkConfig,
+        known_peers: &[Peer],
+        message: Vec<u8>,
+    ) -> Result<(), Error>;
+
+    /// Remains online on the network indefinitely,
+    /// relaying (propagating) messages broadcasted over the network.
+    ///
+    /// * `send` - A channel to send the received messages to.
+    async fn serve(
+        config: &NetworkConfig,
+        peers: Arc<RwLock<Vec<Peer>>>,
+        send: mpsc::Sender<Vec<u8>>,
+    ) -> Result<(), Error>;
 }
+
+/// A **cumulative** set that is shared in the p2p network, backed by the local file system.
+///
+/// One of the notable characteristics of blockchain is that it is based on heights;
+/// The key idea here is that we retanin an instance (both in memory or on disk)
+/// of `DistributedMessageSet` only for a specific height,
+/// and discard if the height progresses, creating a new and empty one again.
+///
+/// For every methods,
+/// - If the given directory is empty, it fails (except `create()`).
+/// - It locks the storage.
+/// - If the given directory is locked (possibly by another instnace of `DistributedMessageSet`),
+/// it will `await` until the lock is released.
+#[async_trait]
+pub trait DistributedMessageSet {
+    /// Creates a new and empty storage with the given directory.
+    /// Fails if there is already a directory.
+    async fn create(storage_directory: &str, height: u64) -> Result<(), Error>;
+
+    /// Fetches the unknown messages from the peers and updates the storage.
+    async fn fetch(
+        network_config: NetworkConfig,
+        known_peers: &[Peer],
+        storage_directory: &str,
+    ) -> Result<(), Error>;
+
+    /// Add the given message to the storage, immediately broadcasting it to the network.
+    async fn add_message(
+        network_config: NetworkConfig,
+        storage_directory: &str,
+        message: Vec<u8>,
+    ) -> Result<(), Error>;
+
+    /// Reads the messages for the storage.
+    async fn read_messages(storage_directory: &str) -> Result<(BlockHeight, Vec<Message>), Error>;
+
+    /// Reads the height for the storage.
+    async fn read_height(storage_directory: &str) -> Result<BlockHeight, Error>;
+
+    /// Advance the height of the message set, discarding all the messages.
+    async fn advance(storage_directory: &str) -> Result<(), Error>;
+
+    /// Serves the p2p network node and the RPC server indefinitely, constantly updating the storage.
+    async fn serve(
+        network_config: NetworkConfig,
+        peers: Arc<RwLock<Vec<Peer>>>,
+        storage_directory: &str,
+    ) -> Result<(), Error>;
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Message {
+    pub data: Vec<u8>,
+    pub author: PublicKey,
+}
+
+/// The interface that will be wrapped into an HTTP RPC server for the peers.
+#[async_trait]
+pub trait DistributedMessageSetRpcInterface {
+    /// Returns the messages except `knowns`. If the height is different, it returns `Err(height)`.
+    async fn get_message(
+        &self,
+        height: BlockHeight,
+        knowns: Vec<Hash256>,
+    ) -> Result<Vec<Message>, BlockHeight>;
+}
+
+// TODO: remove this
+pub trait AuthorizedNetwork: Send + Sync {}
