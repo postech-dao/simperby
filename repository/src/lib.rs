abc3fd0bf01b83278d6732f3e95ead170085661c
Junha Yang <junhayang1@gmail.com>
2022-10-06 23:54:53 +0900

    feat: introduce raw repo

--- repository/src/lib.rs
+++ repository/src/lib.rs
@@ -1,37 +1,11 @@
-use async_trait::async_trait;
-use simperby_common::BlockHeight;
-use simperby_network::*;
-use thiserror::Error;
+pub mod raw;
 
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error("network error: {0}")]
-    Network(simperby_network::Error),
-    #[error("unknown error: {0}")]
-    Unknown(String),
-}
+use serde::{Deserialize, Serialize};
 
-#[async_trait]
-pub trait DistributedRepository {
-    async fn create(directory: &str, height: BlockHeight) -> Result<(), Error>;
+pub type Branch = String;
+pub type Tag = String;
 
-    async fn fetch(
-        directory: &str,
-        network_config: NetworkConfig,
-        known_peers: &[Peer],
-    ) -> Result<(), Error>;
-
-    /// Notifies there was a push for the given repository.
-    async fn notify_push(
-        directory: &str,
-        network_config: NetworkConfig,
-        known_peers: &[Peer],
-    ) -> Result<(), Error>;
-
-    /// Serves the distributed repository protocol indefinitely.
-    async fn serve(
-        directory: &str,
-        network_config: &NetworkConfig,
-        peers: SharedKnownPeers,
-    ) -> Result<tokio::task::JoinHandle<Result<(), Error>>, Error>;
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Serialize, Deserialize, Hash)]
+pub struct CommitHash {
+    pub hash: [u8; 32],
 }
