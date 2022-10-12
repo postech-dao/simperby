588d19a29c60030dda73327798569829c0543b32
Junha Yang <junhayang1@gmail.com>
2022-10-13 02:07:06 +0900

    feat: update SimperbyApi

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -1,245 +1,99 @@
+pub mod node;
+
 pub use simperby_common;
-pub use simperby_kv_storage;
+use simperby_consensus::Consensus;
+use simperby_governance::Governance;
 pub use simperby_network;
 
+use anyhow::Result;
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use simperby_common::crypto::*;
 use simperby_common::*;
-use simperby_kv_storage::KVStorage;
-use thiserror::Error;
-use vetomint::Round;
+use simperby_repository::{raw::RawRepository, CommitHash, DistributedRepository};
 
 pub const PROTOCOL_VERSION: &str = "0.0.0";
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
-pub struct Block {
-    pub header: BlockHeader,
-    pub transactions: Vec<Transaction>,
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Config {
+    pub public_key: PublicKey,
+    pub private_key: PrivateKey,
+    pub chain_name: String,
+    pub p2p_directory: String,
+    pub governance_directory: String,
+    pub consensus_directory: String,
+    pub repository_directory: String,
 }
 
-/// A state transition function.
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ConsensusStatus {
+    // TODO
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct NetworkStatus {
+    // TODO
+}
+
+/// The API for the Simperby node.
+///
+/// It is for serving the **CLI**, providing low-level funcitons and type-specified interfaces.
 #[async_trait]
-pub trait BlockExecutor {
-    type Transaction;
-    async fn execute(
-        &self,
-        storage: &mut dyn KVStorage,
-        transaction: Self::Transaction,
-    ) -> Result<(), ()>;
-}
+pub trait SimperbyApi<RR: RawRepository, R: DistributedRepository<RR>, C: Consensus, G: Governance>
+{
+    /// Initializes a new Simperby node from the genesis state
+    /// stored in the given directory (which is not yet a Git repository).
+    async fn genesis(&self) -> Result<()>;
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
-pub enum StateTransition {
-    AddValidator {
-        /// The public key of the validator.
-        public_key: PublicKey,
-        /// The weight of the validator.
-        voting_power: VotingPower,
-    },
-    RemoveValidator {
-        /// The public key of the validator.
-        public_key: PublicKey,
-    },
-    Delegate {
-        /// The public key of the validator who delegates its voting right.
-        delegator: PublicKey,
-        /// The public key of the validator who is being delegated.
-        delegatee: PublicKey,
-        /// The target height of the block of this transaction, which is for preventing the replay attack.
-        target_height: BlockHeight,
-        /// The signature of (delegatee, target_height) signed by the delegator.
-        ///
-        /// This is a (non-essential) safety guard that assures that the delegator wants this action.
-        /// Otherwise (if this field doesn't exist), any sincere block proposer would include the commitment proof
-        /// in the transaction payload **anyway**, to convince other honest validators.
-        ///
-        /// That's why we just provide a reserved field for that.
-        commitment_proof: TypedSignature<(PublicKey, BlockHeight)>,
-    },
-    Undelegate {
-        /// The public key of the validator who claims its voting right.
-        delegator: PublicKey,
-    },
-    /// Inserts or updates (if exists) an arbitrary data entry.
-    InsertOrUpdateData { key: String, value: Vec<u8> },
-    /// Removes an existing data entry.
-    RemoveData(String),
-    /// Updates the protocol version
-    UpdateVersion { version: String },
-}
-
-/// A unit of state transition of the blockchain.
-///
-/// Unlike other "ordinary" blockchains, Simperby doesn't have a signer nor a signature.
-/// TODO: Explain why.
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
-pub struct Transaction {
-    /// The instruction to perform on the blockchain state.
-    pub state_transition: Option<StateTransition>,
-    /// An optional field to store arbitrary data,
-    /// which is not part of the state but still useful as it's still part of the finalizing blockchain data
-    /// and so can be verified with the Merkle root.
+    /// Initializes a new Simperby node from the repository.
     ///
-    /// Note that it must be `Some` if the `state_transition` is `None`.
-    /// Otherwise, the transaction would be meaningless.
-    pub payload: Option<String>,
-}
+    /// The `main` branch MUST be on a valid block commit.
+    async fn initialize(&self) -> Result<()>;
 
-impl Transaction {
-    pub fn hash(&self) -> Hash256 {
-        Hash256::hash(serde_json::to_vec(self).unwrap())
-    }
-}
+    /// Synchronizes the `main` branch to the given commit.
+    async fn sync(&self, commmit: CommitHash) -> Result<()>;
 
-/// A consensus item that can be voted (thus signed), if the node operator is in favor of it.
-///
-/// Due to the 'interactive' nature of the Simperby consensus,
-/// a typical block validator would manually read the content of `ConsensusVoteItem` and decide whether
-/// it is favorable or not.
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
-pub struct ConsensusVoteItem {
-    /// The hash of the item, which is used as the unique identifier of the it and also used as the sign target.
-    pub hash: Hash256,
-    /// (If exists) The block which is associated with this vote item.
-    pub block: Option<Block>,
-    /// A human-readable description of the item.
-    pub description: String,
-}
+    /// Cleans the repository, removing all the oudated commits.
+    async fn clean(&self, hard: bool) -> Result<()>;
 
-/// A set of operations that may update the Simperby node state.
-///
-/// `ProposeBlock` and `SubmitConsensusVote` are explicitly performed through the `SimperbyApi` trait, and the others
-/// are done implicitly by the background task which is triggered by incoming p2p network messages.
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
-pub enum SimperbyOperation {
-    ProposeBlock {
-        block: BlockHeader,
-        round: Round,
-        prevote_signature: TypedSignature<(BlockHeader, Round)>,
-        timestamp: Timestamp,
-    },
-    SubmitConsensusVote {
-        hash: Hash256,
-        signature: Signature,
-        timestamp: Timestamp,
-    },
-    ReceiveProposal {
-        block: Block,
-        round: Round,
-        author_prevote: TypedSignature<(BlockHeader, Round)>,
-        timestamp: Timestamp,
-    },
-    ReceivePrevote {
-        block_hash: Hash256,
-        round: Round,
-        author_prevote: TypedSignature<(Option<BlockHeader>, Round)>,
-        timestamp: Timestamp,
-    },
-    ReceivePrecommit {
-        block_hash: Hash256,
-        round: Round,
-        author_prevote: TypedSignature<(Option<BlockHeader>, Round)>,
-        timestamp: Timestamp,
-    },
-    ReceiveFinalizedBlock {
-        block: Block,
-        finalization_proof: FinalizationProof,
-        timestamp: Timestamp,
-    },
-    Timer {
-        timestamp: Timestamp,
-    },
-}
+    /// Creates a block commit on the `main` branch.
+    async fn create_block(&self) -> Result<()>;
 
-/// Errors that may occur while accessing the Simperby node.
-#[derive(Error, Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
-pub enum SimperbyError {
-    #[error("invalid block: {0}")]
-    InvalidBlock(String),
-    /// When the operation arguments are not valid.
-    #[error("invalid operation: {0}")]
-    InvalidOperation(String),
-    /// When the underlying storage emits an error.
-    #[error("storage error: {0}")]
-    StorageError(simperby_kv_storage::Error),
-    /// When the data stored in the storage is found to be corrupted.
-    #[error("storage integrity error: {0}")]
-    StorageIntegrityError(String),
-    /// When the consensus safety is violated.
-    #[error("consensus crisis: {0}")]
-    Crisis(String),
-}
+    /// Creates a block commit on the `main` branch.
+    async fn create_agenda(&self) -> Result<()>;
 
-#[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Clone)]
-pub struct SimperbyOperationLog {
-    pub description: String,
-    pub timestamp: simperby_common::Timestamp,
-    pub operation: SimperbyOperation,
-    pub result: Option<SimperbyError>,
-}
+    /// Creates an extra-agenda transaction on the `main` branch.
+    async fn create_extra_agenda_transaction(&self, tx: ExtraAgendaTransaction) -> Result<()>;
 
-#[async_trait]
-pub trait SimperbyApi {
-    /// Gets the genesis info of the blockchain.
-    fn get_genesis_info(&self) -> &GenesisInfo;
+    /// Votes and propagates
+    async fn vote(&self, agenda_commit: CommitHash) -> Result<()>;
 
-    /// Gets the current height of the blockchain.
-    async fn get_height(&self) -> BlockHeight;
+    /// Vetos the current round.
+    async fn veto_round(&self) -> Result<()>;
 
-    /// Gets the finalized block for the given height.
-    async fn get_block(&self, height: BlockHeight) -> Result<Block, SimperbyError>;
+    /// Vetos the given block.
+    async fn veto_block(&self, block_commit: CommitHash) -> Result<()>;
 
-    /// Checks the given block as the next block to be added to the block of the given `height`.
-    ///
-    /// Fails if the block is invalid.
-    async fn check_block(&self, block: Block, height: BlockHeight) -> Result<(), SimperbyError>;
+    /// Runs indefinitely updating everything.
+    async fn run(&self) -> Result<()>;
 
-    /// Reads the finalized state entry by the given key.
-    async fn read_state(&self, key: String, height: BlockHeight) -> Result<Vec<u8>, SimperbyError>;
+    /// Makes a progress for the consensus, returning the result.
+    async fn progress_for_consensus(&self) -> Result<String>;
 
-    /// Gets the current possible consensus voting options.
-    async fn get_consensus_vote_options(&self) -> Result<Vec<ConsensusVoteItem>, SimperbyError>;
-
-    /// Gets the current status of the ongoing consensus.
-    ///
-    /// This is essential because Simperby's consensus is 'interactive', which means that the validator
-    /// has to understand what's going on and manually decide what to do regarding the consensus.
-    ///
-    /// TODO: define the type of the state.
-    async fn get_consensus_status(&self) -> ();
+    /// Gets the current status of the consensus.
+    async fn get_consensus_status(&self) -> Result<ConsensusStatus>;
 
     /// Gets the current status of the p2p network.
-    ///
-    /// Unlike the storage, the p2p network operations are done in the background, in other words,
-    /// no [`SimperbyApi`] method directly accesses the p2p network.
-    /// Thus we need a separate API to see its current status of it.
-    ///
-    /// TODO: define the type of the state.
-    async fn get_network_status(&self) -> ();
+    async fn get_network_status(&self) -> Result<NetworkStatus>;
 
-    /// Gets the last `number` logs of attempts to execute `SimperbyOperation`s.
-    async fn get_operation_log(&self, number: usize) -> Vec<SimperbyOperationLog>;
+    /// Serves indefinitely relaying network messages.
+    async fn relay(&self) -> Result<()>;
 
-    /// Attempts to propose a block for this round. This may update the node state.
-    ///
-    /// It fails
-    /// 1. with the same cause as `check_block`
-    /// 2. if this node is not the current leader.
-    /// 3. if this node has already proposed another block for this round.
-    async fn propose_block(
-        &self,
-        block: Block,
-        round: Round,
-        prevote_signature: TypedSignature<(BlockHeader, Round)>,
-        timestamp: Timestamp,
-    ) -> Result<(), SimperbyError>;
+    /// Fetch the data from the network and apply to the repository, the governance, and the consensus.
+    async fn fetch(&self) -> Result<()>;
 
-    /// Submits a vote for the given item, identified by its hash. This may update the node state.
-    async fn submit_consensus_vote(
-        &self,
-        hash: Hash256,
-        signature: Signature, // This is untyped because the signer doesn't know the source type.
-        timestamp: Timestamp,
-    ) -> Result<(), SimperbyError>;
+    /// Notifies that there was a git push. This is not intended to be used by the user.
+    async fn notify_git_push(&self) -> Result<String>;
+
+    // TODO: Add chat-related methods.
 }
