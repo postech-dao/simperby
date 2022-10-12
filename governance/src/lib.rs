71ca03b855cad75b89477ec8e201296cbbdc015f
Junha Yang <junhayang1@gmail.com>
2022-10-13 02:15:59 +0900

    fix: update governance interface

--- governance/src/lib.rs
+++ governance/src/lib.rs
@@ -1,10 +1,9 @@
+pub mod implementation;
+
 use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
-use simperby_common::{
-    crypto::{Hash256, PublicKey},
-    BlockHeight,
-};
-use simperby_network::*;
+use simperby_common::*;
+use simperby_network::{NetworkConfig, Peer, SharedKnownPeers};
 use std::collections::{HashMap, HashSet};
 use thiserror::Error;
 
@@ -12,8 +11,22 @@ use thiserror::Error;
 pub enum Error {
     #[error("network error: {0}")]
     Network(simperby_network::Error),
+    #[error("crypto error: {0}")]
+    Crypto(CryptoError),
     #[error("unknown error: {0}")]
     Unknown(String),
+}
+
+impl From<simperby_network::Error> for Error {
+    fn from(e: simperby_network::Error) -> Self {
+        Error::Network(e)
+    }
+}
+
+impl From<CryptoError> for Error {
+    fn from(e: CryptoError) -> Self {
+        Error::Crypto(e)
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -24,31 +37,35 @@ pub struct GovernanceState {
 }
 
 #[async_trait]
-pub trait Governance {
+pub trait Governance: Send + Sync {
     async fn create(directory: &str, height: BlockHeight) -> Result<(), Error>;
 
-    async fn read(directory: &str) -> Result<GovernanceState, Error>;
+    async fn open(directory: &str) -> Result<Self, Error>
+    where
+        Self: Sized;
+
+    async fn read(&self) -> Result<GovernanceState, Error>;
 
     async fn vote(
-        directory: &str,
-        network_config: NetworkConfig,
+        &mut self,
+        network_config: &NetworkConfig,
         known_peers: &[Peer],
         agenda_hash: Hash256,
-        height_to_assert: BlockHeight,
+        private_key: &PrivateKey,
     ) -> Result<(), Error>;
 
     /// Advances the block height, discarding all the votes.
-    async fn advance(directory: &str, height_to_assert: BlockHeight) -> Result<(), Error>;
+    async fn advance(&mut self, height_to_assert: BlockHeight) -> Result<(), Error>;
 
     async fn fetch(
-        directory: &str,
-        network_config: NetworkConfig,
+        &mut self,
+        network_config: &NetworkConfig,
         known_peers: &[Peer],
     ) -> Result<(), Error>;
 
     /// Serves the governance protocol indefinitely.
     async fn serve(
-        directory: &str,
+        self,
         network_config: &NetworkConfig,
         peers: SharedKnownPeers,
     ) -> Result<tokio::task::JoinHandle<Result<(), Error>>, Error>;
