c526a6a8e3422a96cd3d0b4ec2824e39a14c30da
Junha Yang <junhayang1@gmail.com>
2022-10-17 23:54:20 +0900

    feat: introduce commit format

---
+++ repository/src/format.rs
@@ -0,0 +1,24 @@
+use crate::raw::SemanticCommit;
+use simperby_common::*;
+
+pub fn to_semantic_commit(commit: &Commit, last_header: &BlockHeader) -> SemanticCommit {
+    match commit {
+        Commit::Agenda(agenda) => {
+            let title = format!("agenda: {}/{}", last_header.height + 1, agenda.to_hash256());
+            let body = serde_json::to_string(agenda).unwrap();
+            SemanticCommit {
+                title,
+                body,
+                reserved_state: None,
+            }
+        }
+        _ => todo!(),
+    }
+}
+
+pub fn from_semantic_commit(
+    _semantic_commit: SemanticCommit,
+    _last_header: &BlockHeader,
+) -> Result<Commit, String> {
+    todo!()
+}
