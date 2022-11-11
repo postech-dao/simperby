60db2c262c59066fa41839caa5ed7ef598cf7c42
JeongHunP <jhp0618@postech.ac.kr>
2022-11-11 14:49:28 +0900

    refactor: change some pattern matching in unit test

--- repository/src/raw.rs
+++ repository/src/raw.rs
@@ -205,6 +205,12 @@ pub trait RawRepository: Send + Sync + 'static {
     ) -> Result<Vec<(String, String, CommitHash)>, Error>;
 }
 
+impl fmt::Debug for RawRepositoryImplInner {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "?")
+    }
+}
+
 pub struct RawRepositoryImplInner {
     repo: Repository,
 }
@@ -681,6 +687,7 @@ impl RawRepositoryImplInner {
     }
 }
 
+#[derive(Debug)]
 pub struct RawRepositoryImpl {
     inner: tokio::sync::Mutex<Option<RawRepositoryImplInner>>,
 }
@@ -878,10 +885,16 @@ mod tests {
     use std::path::Path;
     use tempfile::TempDir;
 
+    const MAIN: &str = "main";
+    const BRANCH_A: &str = "branch_a";
+    const BRANCH_B: &str = "branch_b";
+    const TAG_A: &str = "tag_a";
+    const TAG_B: &str = "tag_b";
+
     /// Make a repository which includes one initial commit at "main" branch.
     /// This returns RawRepositoryImpl containing the repository.
     async fn init_repository_with_initial_commit(path: &Path) -> Result<RawRepositoryImpl, Error> {
-        let repo = RawRepositoryImpl::init(path.to_str().unwrap(), "initial", &("main".to_owned()))
+        let repo = RawRepositoryImpl::init(path.to_str().unwrap(), "initial", &MAIN.into())
             .await
             .unwrap();
 
@@ -895,22 +908,15 @@ mod tests {
         let td = TempDir::new().unwrap();
         let path = td.path();
 
-        let repo = RawRepositoryImpl::init(path.to_str().unwrap(), "initial", &("main".to_owned()))
+        let repo = RawRepositoryImpl::init(path.to_str().unwrap(), "initial", &MAIN.into())
             .await
             .unwrap();
         let branch_list = repo.list_branches().await.unwrap();
+        assert_eq!(branch_list, vec![MAIN.to_owned()]);
 
-        assert_eq!(branch_list.len(), 1);
-        let repo =
-            RawRepositoryImpl::init(path.to_str().unwrap(), "initial", &("main".to_owned())).await;
-        let res = match repo {
-            Ok(_) => "success".to_owned(),
-            Err(_) => "init failure, already repository with same name exists".to_owned(),
-        };
-        assert_eq!(
-            res,
-            "init failure, already repository with same name exists".to_owned()
-        );
+        let repo = RawRepositoryImpl::init(path.to_str().unwrap(), "initial", &MAIN.into())
+            .await
+            .unwrap_err();
     }
 
     /// Open existed repository and verifies whether it opens well.
@@ -928,17 +934,16 @@ mod tests {
         let branch_list_init = init_repo.list_branches().await.unwrap();
         let branch_list_open = open_repo.list_branches().await.unwrap();
 
-        assert_eq!(branch_list_init.len(), branch_list_open.len());
-        assert_eq!(branch_list_init[0], branch_list_open[0]);
+        assert_eq!(branch_list_init, branch_list_open);
     }
 
     /*
-       c2 (HEAD -> main)      c2 (HEAD -> main, branch_1)     c2 (HEAD -> main)
+       c2 (HEAD -> main)      c2 (HEAD -> main, branch_a)     c2 (HEAD -> main)
        |                -->   |                          -->  |
-       c1 (branch_1)          c1                              c1
+       c1 (branch_a)          c1                              c1
     */
-    /// Create "branch_1" at c1, create c2 at "main" branch and move "branch_1" head from c1 to c2.
-    /// Finally, "branch_1" is removed.
+    /// Create "branch_a" at c1, create c2 at "main" branch and move "branch_a" head from c1 to c2.
+    /// Finally, "branch_a" is removed.
     #[ignore]
     #[tokio::test]
     async fn branch() {
@@ -948,49 +953,37 @@ mod tests {
 
         // There is one branch "main" at initial state
         let branch_list = repo.list_branches().await.unwrap();
-        assert_eq!(branch_list.len(), 1);
-        assert_eq!(branch_list[0], "main".to_owned());
+        assert_eq!(branch_list, vec![MAIN.to_owned()]);
 
-        // git branch branch_1
+        // git branch branch_a
         let head = repo.get_head().await.unwrap();
-        repo.create_branch(&("branch_1".to_owned()), head)
-            .await
-            .unwrap();
+        repo.create_branch(&BRANCH_A.into(), head).await.unwrap();
 
         // "branch_list" is sorted by branch names' alphabetic order
         let branch_list = repo.list_branches().await.unwrap();
-        assert_eq!(branch_list.len(), 2);
-        assert_eq!(branch_list[0], "branch_1".to_owned());
-        assert_eq!(branch_list[1], "main".to_owned());
+        assert_eq!(branch_list, vec![BRANCH_A.to_owned(), MAIN.to_owned()]);
 
-        let branch_1_commit_hash = repo.locate_branch(&("branch_1".to_owned())).await.unwrap();
-        assert_eq!(branch_1_commit_hash, head);
+        let branch_a_commit_hash = repo.locate_branch(&BRANCH_A.into()).await.unwrap();
+        assert_eq!(branch_a_commit_hash, head);
 
         // Make second commit with "main" branch
         let _commit = repo.create_commit("second", Some("")).await.unwrap();
 
-        // Move "branch_1" head to "main" head
-        let main_commit_hash = repo.locate_branch(&("main".to_owned())).await.unwrap();
-        repo.move_branch(&("branch_1".to_owned()), &main_commit_hash)
+        // Move "branch_a" head to "main" head
+        let main_commit_hash = repo.locate_branch(&MAIN.into()).await.unwrap();
+        repo.move_branch(&BRANCH_A.into(), &main_commit_hash)
             .await
             .unwrap();
-        let branch_1_commit_hash = repo.locate_branch(&("branch_1".to_owned())).await.unwrap();
-        assert_eq!(main_commit_hash, branch_1_commit_hash);
+        let branch_a_commit_hash = repo.locate_branch(&BRANCH_A.into()).await.unwrap();
+        assert_eq!(main_commit_hash, branch_a_commit_hash);
 
-        // Remove "branch_1" and the remaining branch should be only "main"
-        repo.delete_branch(&("branch_1".to_owned())).await.unwrap();
+        // Remove "branch_a" and the remaining branch should be only "main"
+        repo.delete_branch(&BRANCH_A.into()).await.unwrap();
         let branch_list = repo.list_branches().await.unwrap();
-        assert_eq!(branch_list.len(), 1);
-        assert_eq!(branch_list[0], "main".to_owned());
+        assert_eq!(branch_list, vec![MAIN.to_owned()]);
 
-        // TODO: Change match if possible
         // This fails since current HEAD points at "main" branch
-        let remove_main = repo.delete_branch(&("main".to_owned())).await;
-        let res = match remove_main {
-            Ok(_) => "success".to_owned(),
-            Err(_) => "failure".to_owned(),
-        };
-        assert_eq!(res, "failure".to_owned());
+        let remove_main = repo.delete_branch(&MAIN.into()).await.unwrap_err();
     }
 
     /// Create a tag and remove it.
@@ -1003,32 +996,31 @@ mod tests {
 
         // There is no tags at initial state
         let tag_list = repo.list_tags().await.unwrap();
-        assert_eq!(tag_list.len(), 0);
+        assert!(tag_list.is_empty());
 
         // Create "tag_1" at first commit
-        let first_commit_hash = repo.locate_branch(&("main".to_owned())).await.unwrap();
-        repo.create_tag(&("tag_1".to_owned()), &first_commit_hash)
+        let first_commit_hash = repo.locate_branch(&MAIN.into()).await.unwrap();
+        repo.create_tag(&TAG_A.into(), &first_commit_hash)
             .await
             .unwrap();
         let tag_list = repo.list_tags().await.unwrap();
-        assert_eq!(tag_list.len(), 1);
-        assert_eq!(tag_list[0], "tag_1".to_owned());
+        assert_eq!(tag_list, vec![TAG_A.to_owned()]);
 
-        let tag_1_commit_hash = repo.locate_tag(&("tag_1".to_owned())).await.unwrap();
-        assert_eq!(first_commit_hash, tag_1_commit_hash);
+        let tag_a_commit_hash = repo.locate_tag(&TAG_A.into()).await.unwrap();
+        assert_eq!(first_commit_hash, tag_a_commit_hash);
 
         // Remove "tag_1"
-        repo.remove_tag(&("tag_1".to_owned())).await.unwrap();
+        repo.remove_tag(&TAG_A.into()).await.unwrap();
         let tag_list = repo.list_tags().await.unwrap();
-        assert_eq!(tag_list.len(), 0);
+        assert!(tag_list.is_empty());
     }
 
     /*
         c3 (HEAD -> main)   c3 (HEAD -> main)     c3 (main)                   c3 (HEAD -> main)
         |                   |                     |                           |
-        c2 (branch_2)  -->  c2 (branch_2)  -->    c2 (HEAD -> branch_2)  -->  c2 (branch_2)
+        c2 (branch_b)  -->  c2 (branch_b)  -->    c2 (HEAD -> branch_b)  -->  c2 (branch_b)
         |                   |                     |                           |
-        c1 (branch_1)       c1 (HEAD -> branch_1) c1 (branch_1)               c1 (branch_1)
+        c1 (branch_a)       c1 (HEAD -> branch_a) c1 (branch_a)               c1 (branch_a)
     */
     /// Checkout to each commits with different branches.
     #[ignore]
@@ -1039,32 +1031,32 @@ mod tests {
         let mut repo = init_repository_with_initial_commit(path).await.unwrap();
 
         // TODO: Should change after "create_commit" is changed
-        // Create branch_1 at c1 and commit c2
-        let first_commit_hash = repo.locate_branch(&("main".to_owned())).await.unwrap();
-        repo.create_branch(&("branch_1".to_owned()), first_commit_hash)
+        // Create branch_a at c1 and commit c2
+        let first_commit_hash = repo.locate_branch(&MAIN.into()).await.unwrap();
+        repo.create_branch(&BRANCH_A.into(), first_commit_hash)
             .await
             .unwrap();
         let _commit = repo.create_commit("second", Some("")).await.unwrap();
-        // Create branch_2 at c2 and commit c3
-        let second_commit_hash = repo.locate_branch(&("main".to_owned())).await.unwrap();
-        repo.create_branch(&("branch_2".to_owned()), second_commit_hash)
+        // Create branch_b at c2 and commit c3
+        let second_commit_hash = repo.locate_branch(&MAIN.into()).await.unwrap();
+        repo.create_branch(&BRANCH_B.into(), second_commit_hash)
             .await
             .unwrap();
         let _commit = repo.create_commit("third", Some("")).await.unwrap();
 
-        let first_commit_hash = repo.locate_branch(&("branch_1".to_owned())).await.unwrap();
-        let second_commit_hash = repo.locate_branch(&("branch_2".to_owned())).await.unwrap();
-        let third_commit_hash = repo.locate_branch(&("main".to_owned())).await.unwrap();
+        let first_commit_hash = repo.locate_branch(&BRANCH_A.into()).await.unwrap();
+        let second_commit_hash = repo.locate_branch(&BRANCH_B.into()).await.unwrap();
+        let third_commit_hash = repo.locate_branch(&MAIN.into()).await.unwrap();
 
-        // Checkout to branch_1, branch_2, main sequentially
+        // Checkout to branch_a, branch_b, main sequentially
         // Compare the head's commit hash after checkout with each branch's commit hash
-        repo.checkout(&("branch_1".to_owned())).await.unwrap();
+        repo.checkout(&BRANCH_A.into()).await.unwrap();
         let head_commit_hash = repo.get_head().await.unwrap();
         assert_eq!(head_commit_hash, first_commit_hash);
-        repo.checkout(&("branch_2".to_owned())).await.unwrap();
+        repo.checkout(&BRANCH_B.into()).await.unwrap();
         let head_commit_hash = repo.get_head().await.unwrap();
         assert_eq!(head_commit_hash, second_commit_hash);
-        repo.checkout(&("main".to_owned())).await.unwrap();
+        repo.checkout(&MAIN.into()).await.unwrap();
         let head_commit_hash = repo.get_head().await.unwrap();
         assert_eq!(head_commit_hash, third_commit_hash);
     }
@@ -1084,18 +1076,17 @@ mod tests {
 
         // There is one branch "main" at initial state
         let branch_list = repo.list_branches().await.unwrap();
-        assert_eq!(branch_list.len(), 1);
-        assert_eq!(branch_list[0], "main".to_owned());
+        assert_eq!(branch_list, vec![MAIN.to_owned()]);
 
-        let commit1 = repo.get_head().await.unwrap();
+        let first_commit_hash = repo.get_head().await.unwrap();
         // Make second commit with "main" branch
-        let _commit = repo.create_commit("second", Some("")).await.unwrap();
+        repo.create_commit("second", Some("")).await.unwrap();
 
-        // Checkout to commit1 and set HEAD detached mode
-        repo.checkout_detach(&commit1).await.unwrap();
+        // Checkout to c1 and set HEAD detached mode
+        repo.checkout_detach(&first_commit_hash).await.unwrap();
 
         let cur_head_commit_hash = repo.get_head().await.unwrap();
-        assert_eq!(cur_head_commit_hash, commit1);
+        assert_eq!(cur_head_commit_hash, first_commit_hash);
 
         // TODO: Create a function of getting head name(see below).
         // This means the current head is at a detached mode,
@@ -1121,13 +1112,10 @@ mod tests {
         let path = td.path();
         let mut repo = init_repository_with_initial_commit(path).await.unwrap();
 
-        // Create branch_1, branch_2 and commits
-        let first_commit_hash = repo.locate_branch(&("main".to_owned())).await.unwrap();
-        println!("{:?}", first_commit_hash);
-        let _commit = repo.create_commit("second", Some("")).await.unwrap();
-        println!("{:?}", _commit);
-        let _commit = repo.create_commit("third", Some("")).await.unwrap();
-        println!("{:?}", _commit);
+        // Create branch_a, branch_b and commits
+        let first_commit_hash = repo.locate_branch(&MAIN.into()).await.unwrap();
+        repo.create_commit("second", Some("")).await.unwrap();
+        repo.create_commit("third", Some("")).await.unwrap();
 
         let initial_commit_hash = repo.get_initial_commit().await.unwrap();
         assert_eq!(initial_commit_hash, first_commit_hash);
@@ -1149,34 +1137,29 @@ mod tests {
         let path = td.path();
         let mut repo = init_repository_with_initial_commit(path).await.unwrap();
 
-        let first_commit_hash = repo.locate_branch(&("main".to_owned())).await.unwrap();
-        let _commit = repo.create_commit("second", Some("")).await.unwrap();
-        let second_commit_hash = repo.locate_branch(&("main".to_owned())).await.unwrap();
+        let first_commit_hash = repo.locate_branch(&MAIN.into()).await.unwrap();
+        repo.create_commit("second", Some("")).await.unwrap();
+        let second_commit_hash = repo.locate_branch(&MAIN.into()).await.unwrap();
         // Make second commit at "main" branch
-        let third_commit_hash = repo.locate_branch(&("main".to_owned())).await.unwrap();
+        let third_commit_hash = repo.locate_branch(&MAIN.into()).await.unwrap();
 
         // Get only one ancestor(direct parent)
         let ancestors = repo
             .list_ancestors(&third_commit_hash, Some(1))
             .await
             .unwrap();
-        assert_eq!(ancestors.len(), 1);
-        assert_eq!(ancestors[0], second_commit_hash);
+        assert_eq!(ancestors, vec![second_commit_hash]);
 
         // Get two ancestors with max 2
         let ancestors = repo
             .list_ancestors(&third_commit_hash, Some(2))
             .await
             .unwrap();
-        assert_eq!(ancestors.len(), 2);
-        assert_eq!(ancestors[0], second_commit_hash);
-        assert_eq!(ancestors[1], first_commit_hash);
+        assert_eq!(ancestors, vec![second_commit_hash, first_commit_hash]);
 
         // Get all ancestors
         let ancestors = repo.list_ancestors(&third_commit_hash, None).await.unwrap();
-        assert_eq!(ancestors.len(), 2);
-        assert_eq!(ancestors[0], second_commit_hash);
-        assert_eq!(ancestors[1], first_commit_hash);
+        assert_eq!(ancestors, vec![second_commit_hash, first_commit_hash]);
 
         // TODO: If max num > the number of ancestors
     }
@@ -1197,32 +1180,32 @@ mod tests {
 
         // Create "branch_a" and "branch_b" branches at c1
         {
-            let commit_hash1 = repo.locate_branch(&("main".to_owned())).await.unwrap();
-            repo.create_branch(&("branch_a".to_owned()), commit_hash1)
+            let commit_hash1 = repo.locate_branch(&MAIN.into()).await.unwrap();
+            repo.create_branch(&BRANCH_A.into(), commit_hash1)
                 .await
                 .unwrap();
-            repo.create_branch(&("branch_b".to_owned()), commit_hash1)
+            repo.create_branch(&BRANCH_B.into(), commit_hash1)
                 .await
                 .unwrap();
         }
         // Make a commit at "branch_a" branch
-        repo.checkout(&("branch_a".to_owned())).await.unwrap();
+        repo.checkout(&BRANCH_A.into()).await.unwrap();
         let _commit = repo.create_commit("branch_a", Some("")).await.unwrap();
         // Make a commit at "branch_b" branch
-        repo.checkout(&("branch_b".to_owned())).await.unwrap();
+        repo.checkout(&BRANCH_B.into()).await.unwrap();
         let _commit = repo.create_commit("branch_b", Some("")).await.unwrap();
 
         // Make merge base of (c2,c3)
-        let commit_hash1 = repo.locate_branch(&("main".to_owned())).await.unwrap();
-        let commit_hash_a = repo.locate_branch(&("branch_a".to_owned())).await.unwrap();
-        let commit_hash_b = repo.locate_branch(&("branch_b".to_owned())).await.unwrap();
+        let commit_hash_main = repo.locate_branch(&MAIN.into()).await.unwrap();
+        let commit_hash_a = repo.locate_branch(&BRANCH_A.into()).await.unwrap();
+        let commit_hash_b = repo.locate_branch(&BRANCH_B.into()).await.unwrap();
         let merge_base = repo
             .find_merge_base(&commit_hash_a, &commit_hash_b)
             .await
             .unwrap();
 
         // The merge base of (c2,c3) should be c1
-        assert_eq!(merge_base, commit_hash1);
+        assert_eq!(merge_base, commit_hash_main);
     }
 
     /// add remote repository and remove it.
@@ -1237,13 +1220,14 @@ mod tests {
         repo.add_remote("origin", "/path/to/nowhere").await.unwrap();
 
         let remote_list = repo.list_remotes().await.unwrap();
-        assert_eq!(remote_list.len(), 1);
-        assert_eq!(remote_list[0].0, "origin".to_owned());
-        assert_eq!(remote_list[0].1, "/path/to/nowhere".to_owned());
+        assert_eq!(
+            remote_list,
+            vec![("origin".to_owned(), "/path/to/nowhere".to_owned())]
+        );
 
         // Remove dummy remote
         repo.remove_remote("origin").await.unwrap();
         let remote_list = repo.list_remotes().await.unwrap();
-        assert_eq!(remote_list.len(), 0);
+        assert!(remote_list.is_empty());
     }
 }
