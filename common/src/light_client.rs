1ef533e2b868fe5f8f23727d5ccb9cfe0c8aea44
TomTaehoonKim <taehoontomkim@gmail.com>
2022-11-16 12:06:18 +0900

    fix: update `LightClient`

--- common/src/light_client.rs
+++ common/src/light_client.rs
@@ -12,8 +12,8 @@ pub struct MerkleProof {
 pub struct LightClient {
     pub repository_roots: Vec<Hash256>,
     pub state_roots_height_offset: u64,
-    pub tx_roots: Vec<Hash256>,
-    pub tx_roots_height_offset: u64,
+    pub commit_roots: Vec<Hash256>,
+    pub commit_roots_height_offset: u64,
     pub last_header: BlockHeader,
 }
 
@@ -23,8 +23,8 @@ impl LightClient {
         Self {
             repository_roots: vec![initial_header.repository_merkle_root],
             state_roots_height_offset: initial_header.height,
-            tx_roots: vec![initial_header.tx_merkle_root],
-            tx_roots_height_offset: initial_header.height,
+            commit_roots: vec![initial_header.commit_merkle_root],
+            commit_roots_height_offset: initial_header.height,
             last_header: initial_header,
         }
     }
@@ -34,7 +34,7 @@ impl LightClient {
         verify::verify_header_to_header(&self.last_header, &header).map_err(|e| e.to_string())?;
         verify::verify_finalization_proof(&header, &proof).map_err(|e| e.to_string())?;
         self.repository_roots.push(header.repository_merkle_root);
-        self.tx_roots.push(header.tx_merkle_root);
+        self.commit_roots.push(header.commit_merkle_root);
         self.last_header = header;
         Ok(())
     }
