6b726fe574b19fce3fbef806be0751677000de9c
Junha Yang <junhayang1@gmail.com>
2022-11-21 04:15:29 +0900

    Add simple test for DMS

--- network/src/primitives.rs
+++ network/src/primitives.rs
@@ -72,3 +72,35 @@ pub trait GossipNetwork: Send + Sync + 'static {
         Error,
     >;
 }
+
+pub struct DummyGossipNetwork;
+
+#[async_trait]
+impl GossipNetwork for DummyGossipNetwork {
+    async fn broadcast(
+        _config: &NetworkConfig,
+        _known_peers: &[Peer],
+        _message: Vec<u8>,
+    ) -> Result<(), Error> {
+        Ok(())
+    }
+
+    async fn serve(
+        _config: NetworkConfig,
+        _peers: SharedKnownPeers,
+    ) -> Result<
+        (
+            mpsc::Receiver<Vec<u8>>,
+            tokio::task::JoinHandle<Result<(), Error>>,
+        ),
+        Error,
+    > {
+        let (send, recv) = mpsc::channel(1);
+        let task = tokio::spawn(async move {
+            let _send = send;
+            tokio::time::sleep(std::time::Duration::MAX).await;
+            Ok(())
+        });
+        Ok((recv, task))
+    }
+}
