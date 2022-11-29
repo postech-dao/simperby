7a83eb3500d49160d621749cfdc4ac5e5ebfc7ad
Junha Yang <junhayang1@gmail.com>
2022-11-30 01:00:17 +0900

    hmm

--- vetomint/tests/integration_test.rs
+++ vetomint/tests/integration_test.rs
@@ -3,6 +3,109 @@ mod unit_action;
 use unit_action::*;
 use vetomint::*;
 
+/// A very normal, desirable, and expected scenario.
+#[test]
+fn normal_1() {
+    let mut height_info = HeightInfo {
+        validators: vec![1, 1, 1, 1],
+        this_node_index: Some(0),
+        timestamp: 0,
+        consensus_params: ConsensusParams {
+            timeout_ms: 100,
+            repeat_round_for_first_leader: 1,
+        },
+        initial_block_candidate: 0,
+    };
+    let mut proposer = ConsensusState::new(height_info.clone());
+    let mut nodes = Vec::new();
+    for i in 1..=3 {
+        height_info.this_node_index = Some(i);
+        nodes.push(ConsensusState::new(height_info.clone()));
+    }
+    let response = proposer.progress(ConsensusEvent::Start { time: 0 });
+    assert_eq!(
+        response,
+        Some(vec![ConsensusResponse::BroadcastProposal {
+            proposal: 0,
+            valid_round: None,
+            round: 0,
+        },])
+    );
+    for node in nodes.iter_mut() {
+        let response = node.progress(ConsensusEvent::Start { time: 0 });
+        assert_eq!(response, Some(vec![]));
+    }
+    for node in nodes.iter_mut() {
+        let response = node.progress(ConsensusEvent::BlockProposalReceived {
+            proposal: 0,
+            valid_round: None,
+            proposer: 0,
+            round: 0,
+            time: 1,
+            favor: true,
+        });
+        assert_eq!(
+            response,
+            Some(vec![ConsensusResponse::BroadcastNonNilPrevote {
+                proposal: 0,
+                round: 0,
+            }])
+        );
+    }
+
+    let mut nodes = vec![vec![proposer], nodes].concat();
+
+    for (i, node) in nodes.iter_mut().enumerate() {
+        let response = node.progress(ConsensusEvent::NonNilPrevote {
+            proposal: 0,
+            signer: (i + 1) % 4,
+            round: 0,
+            time: 2,
+        });
+        assert_eq!(response, None);
+        let response = node.progress(ConsensusEvent::NonNilPrevote {
+            proposal: 0,
+            signer: (i + 2) % 4,
+            round: 0,
+            time: 2,
+        });
+        assert_eq!(
+            response,
+            Some(vec![ConsensusResponse::BroadcastNonNilPrecommit {
+                proposal: 0,
+                round: 0,
+            }])
+        );
+        let response = node.progress(ConsensusEvent::NonNilPrevote {
+            proposal: 0,
+            signer: (i + 3) % 4,
+            round: 0,
+            time: 2,
+        });
+        assert_eq!(response, Some(Vec::new()));
+    }
+
+    for (i, node) in nodes.iter_mut().enumerate() {
+        let response = node.progress(ConsensusEvent::NonNilPrecommit {
+            proposal: 0,
+            signer: (i + 1) % 4,
+            round: 0,
+            time: 2,
+        });
+        assert_eq!(response, Some(Vec::new()));
+        let response = node.progress(ConsensusEvent::NonNilPrecommit {
+            proposal: 0,
+            signer: (i + 2) % 4,
+            round: 0,
+            time: 2,
+        });
+        assert_eq!(
+            response,
+            Some(vec![ConsensusResponse::FinalizeBlock { proposal: 0 }])
+        );
+    }
+}
+
 /// 4f+1 prvote and polka
 #[test]
 fn early_termination_by_polka_1() {
