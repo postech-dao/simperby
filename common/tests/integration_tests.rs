d3890258df2d28174e61999f1bd5054c77f641e0
Junha Yang <junhayang1@gmail.com>
2022-12-06 21:49:39 +0900

    Introduce Simperby test suite

--- common/tests/integration_tests.rs
+++ common/tests/integration_tests.rs
@@ -1,55 +1,14 @@
 use light_client::*;
 use merkle_tree::*;
-use reserved::ReservedState;
 use simperby_common::{verify::CommitSequenceVerifier, *};
+use simperby_test_suite::*;
 
 #[test]
 fn basic1() {
     let n = 10;
-    let keys = (0..n)
-        .into_iter()
-        .map(|i| generate_keypair(format!("{}", i)))
-        .collect::<Vec<_>>();
-    let members = keys
-        .iter()
-        .enumerate()
-        .map(|(i, (public_key, _))| Member {
-            public_key: public_key.clone(),
-            name: format!("member-{}", i),
-            governance_voting_power: 1,
-            consensus_voting_power: 1,
-            governance_delegations: None,
-            consensus_delegations: None,
-        })
-        .collect::<Vec<_>>();
-    let genesis_header = BlockHeader {
-        author: PublicKey::zero(),
-        prev_block_finalization_proof: Vec::new(),
-        previous_hash: Hash256::zero(),
-        height: 0,
-        timestamp: 0,
-        commit_merkle_root: Hash256::zero(),
-        repository_merkle_root: Hash256::zero(),
-        validator_set: members
-            .iter()
-            .map(|member| (member.public_key.clone(), member.consensus_voting_power))
-            .collect::<Vec<_>>(),
-        version: "0.1.0".to_string(),
-    };
-    let genesis_info = GenesisInfo {
-        header: genesis_header.clone(),
-        genesis_proof: keys
-            .iter()
-            .map(|(_, private_key)| TypedSignature::sign(&genesis_header, private_key).unwrap())
-            .collect::<Vec<_>>(),
-        chain_name: "test-chain".to_string(),
-    };
-    let rs = ReservedState {
-        genesis_info: genesis_info.clone(),
-        members,
-        consensus_leader_order: (0..n).into_iter().collect::<Vec<_>>(),
-        version: "0.1.0".to_string(),
-    };
+    let (rs, keys) = generate_standard_genesis(n);
+    let genesis_info = rs.genesis_info.clone();
+    let genesis_header = rs.genesis_info.header.clone();
 
     let mut csv = CommitSequenceVerifier::new(genesis_header.clone(), rs).unwrap();
     let mut light_client = LightClient::new(genesis_header);
