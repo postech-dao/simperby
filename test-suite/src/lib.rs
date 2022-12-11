b3c1a8f33598bb12f0d82ec8b5cc84281677a01d
YoonhoKim0527 <hjyang093@gmail.com>
2022-12-11 21:44:00 +0900

    Add integration test

--- test-suite/src/lib.rs
+++ test-suite/src/lib.rs
@@ -106,6 +106,80 @@ pub fn generate_standard_genesis(
     )
 }
 
+/// Generates a standard test chain config returning the genesis reserved-state
+/// and the associated key pairs of the members.
+pub fn generate_delegated_genesis(
+    member_number: usize,
+) -> (ReservedState, Vec<(PublicKey, PrivateKey)>) {
+    let keys = (0..member_number)
+        .into_iter()
+        .map(|i| generate_keypair(format!("{}", i)))
+        .collect::<Vec<_>>();
+    // member-0000 delegates to member-0002 both for governance and consensus
+    let members = keys
+        .iter()
+        .enumerate()
+        .map(|(i, (public_key, _))| Member {
+            public_key: public_key.clone(),
+            // lexicographically ordered
+            name: format!("member-{:04}", i),
+            governance_voting_power: 1,
+            consensus_voting_power: 1,
+            governance_delegations: if i == 0 {
+                Some("member-0002".into())
+            } else {
+                None
+            },
+            consensus_delegations: if i == 0 {
+                Some("member-0002".into())
+            } else {
+                None
+            },
+        })
+        .collect::<Vec<_>>();
+    // remove key of member-0000
+    let keys = keys
+        .into_iter()
+        .enumerate()
+        .filter(|(i, _)| *i != 0)
+        .map(|(_, key)| key)
+        .collect::<Vec<_>>();
+    let genesis_header = BlockHeader {
+        author: PublicKey::zero(),
+        prev_block_finalization_proof: Vec::new(),
+        previous_hash: Hash256::zero(),
+        height: 0,
+        timestamp: 0,
+        commit_merkle_root: Hash256::zero(),
+        repository_merkle_root: Hash256::zero(),
+        validator_set: members
+            .iter()
+            .map(|member| (member.public_key.clone(), member.consensus_voting_power))
+            .collect::<Vec<_>>(),
+        version: "0.1.0".to_string(),
+    };
+    let genesis_info = GenesisInfo {
+        header: genesis_header.clone(),
+        genesis_proof: keys
+            .iter()
+            .map(|(_, private_key)| TypedSignature::sign(&genesis_header, private_key).unwrap())
+            .collect::<Vec<_>>(),
+        chain_name: "test-chain".to_string(),
+    };
+    (
+        ReservedState {
+            genesis_info,
+            members,
+            consensus_leader_order: (1..member_number)
+                .into_iter()
+                .map(|i| format!("member-{:04}", i))
+                .collect::<Vec<_>>(),
+            version: "0.1.0".to_string(),
+        },
+        keys,
+    )
+}
+
 /// Creates a `repository` directory inside the given directory
 /// and initializes a pre-genesis repository.
 pub async fn setup_pre_genesis_repository(path: &str, reserved_state: ReservedState) {
