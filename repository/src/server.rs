ae968c1acf71eb19d391fa2153396d99b6e00e40
JeongHunP <jhp0618@postech.ac.kr>
2023-01-30 18:52:29 +0900

    Make hook receive specific branch

--- repository/src/server.rs
+++ repository/src/server.rs
@@ -41,17 +41,17 @@ pub async fn run_server_legacy(path: &str, port: u16) -> GitServer {
     let pid_path = format!("{}/pid", td.path().to_slash().unwrap().into_owned());
     let child = std::process::Command::new("git")
         .arg("daemon")
-        .arg(format!("--base-path={}", path))
+        .arg(format!("--base-path={path}"))
         .arg("--export-all")
-        .arg(format!("--port={}", port))
-        .arg(format!("--pid-file={}", pid_path))
+        .arg(format!("--port={port}"))
+        .arg(format!("--pid-file={pid_path}"))
         .spawn()
         .unwrap();
     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     let daemon_pid = std::fs::read_to_string(pid_path).unwrap();
     // remove new line character
     let daemon_pid = daemon_pid[0..daemon_pid.len() - 1].parse::<u32>().unwrap();
-    println!("PID: {}", daemon_pid);
+    println!("PID: {daemon_pid}");
     GitServer { child, daemon_pid }
 }
 
@@ -62,8 +62,8 @@ pub async fn run_server_legacy(path: &str, port: u16) -> GitServer {
 /// - `simperby_executable_path` is the path to the Simperby executable, which will be executed by the hook.
 pub async fn run_server(path: &str, port: u16, simperby_executable_path: &str) -> GitServer {
     fs::rename(
-        format!("{}/repository/repo/.git/hooks/pre-receive.sample", path),
-        format!("{}/repository/repo/.git/hooks/pre-receive", path),
+        format!("{path}/repository/repo/.git/hooks/pre-receive.sample"),
+        format!("{path}/repository/repo/.git/hooks/pre-receive"),
     )
     .await
     .unwrap();
@@ -71,7 +71,7 @@ pub async fn run_server(path: &str, port: u16, simperby_executable_path: &str) -
     // TODO: pre_receive.sh should be modified after Simperby executable is made.
     let hook_content = include_str!("pre_receive.sh");
     fs::write(
-        format!("{}/repository/repo/.git/hooks/pre-receive", path),
+        format!("{path}/repository/repo/.git/hooks/pre-receive"),
         hook_content,
     )
     .await
@@ -82,10 +82,11 @@ pub async fn run_server(path: &str, port: u16, simperby_executable_path: &str) -
 
     let child = std::process::Command::new("git")
         .arg("daemon")
-        .arg(format!("--base-path={}/repository", path))
+        .arg(format!("--base-path={path}/repository"))
         .arg("--export-all")
-        .arg(format!("--port={}", port))
-        .arg(format!("--pid-file={}", pid_path))
+        .arg("--enable=receive-pack")
+        .arg(format!("--port={port}"))
+        .arg(format!("--pid-file={pid_path}"))
         .env("SIMPERBY_PATH", simperby_executable_path)
         .spawn()
         .unwrap();
@@ -146,58 +147,54 @@ mod tests {
         let path_server = td_server.path().to_slash().unwrap().into_owned();
 
         run_command(format!(
-            "cd {} && mkdir repository && cd repository && mkdir repo && cd repo && git init",
-            path_server
+            "cd {path_server} && mkdir repository && cd repository && mkdir repo && cd repo && git init"
         ))
         .await;
         run_command(format!(
-            "cd {}/repository/repo && git config user.name 'Test' && git config user.email 'test@test.com'",
-            path_server
+            "cd {path_server}/repository/repo && git config user.name 'Test' && git config user.email 'test@test.com'"
+        ))
+        .await;
+        run_command(format!(
+            "cd {path_server}/repository/repo && git config receive.advertisePushOptions true"
         ))
         .await;
 
-        // TODO: change config.
         run_command(format!(
-            "cd {}/repository/repo && git config receive.advertisePushOptions true",
-            path_server
-        ))
-        .await;
-        run_command(format!(
-            "cd {}/repository/repo && echo 'init' > init.txt",
-            path_server
-        ))
-        .await;
-        run_command(format!("cd {}/repository/repo && git add -A", path_server)).await;
-        run_command(format!(
-            "cd {}/repository/repo && git commit -m 'init'",
-            path_server
+            "cd {path_server}/repository/repo && echo 'init' > init.txt && git add -A && git commit -m 'init'"
         ))
         .await;
 
         let td_simperby = TempDir::new().unwrap();
-        let simperby_executable_path = td_simperby.path().to_slash().unwrap().into_owned();
+        let path_simperby = td_simperby.path().to_slash().unwrap().into_owned();
 
         // Make .sh example file for testing the server hook.
-        let path_cli = format!("{}/simperby_cli_example.sh", simperby_executable_path);
-        fs::File::create(&path_cli).await.unwrap();
+        let path_simperby = format!("{path_simperby}/simperby_cli_example.sh");
+        fs::File::create(&path_simperby).await.unwrap();
         let cli_content = r#"#!/bin/sh
 string=$1
+branch_name=$2
 result=true
+
 case "$string" in
 reject)
     result=false
     ;;
 esac
 
-echo "$result"
+if [ $branch_name != "work" ]
+then 
+result=false
+fi
+
+echo $result
 "#;
-        fs::write(&path_cli, cli_content).await.unwrap();
-        fs::set_permissions(&path_cli, std::fs::Permissions::from_mode(0o755))
+        fs::write(&path_simperby, cli_content).await.unwrap();
+        fs::set_permissions(&path_simperby, std::fs::Permissions::from_mode(0o755))
             .await
             .unwrap();
 
         let server_task = tokio::spawn(async move {
-            let _x = run_server(&path_server, port, &simperby_executable_path).await;
+            let _x = run_server(&path_server, port, &path_simperby).await;
             sleep_ms(6000).await;
         });
         tokio::time::sleep(std::time::Duration::from_secs(4)).await;
@@ -205,41 +202,33 @@ echo "$result"
         // Make a local repository by cloning above server repository.
         let td_local = TempDir::new().unwrap();
         let path_local = td_local.path().to_slash().unwrap().into_owned();
-        run_command(format!("ls {}", path_local)).await;
+        run_command(format!("ls {path_local}",)).await;
         run_command(format!(
-            "cd {} && git clone git://127.0.0.1:{}/repo",
-            path_local, port
+            "cd {path_local} && git clone git://127.0.0.1:{port}/repo"
         ))
         .await;
         run_command(format!(
-            "cd {}/repo && git config user.name 'Test2' && git config user.email 'test2@test.com'",
-            path_local
+            "cd {path_local}/repo && git config user.name 'Test2' && git config user.email 'test2@test.com'"
         ))
         .await;
-        run_command(format!("cd {}/repo && git branch test", path_local)).await;
-        run_command(format!("cd {}/repo && git checkout test", path_local)).await;
-
         run_command(format!(
-            "cd {}/repo && echo 'hello' > hello.txt",
-            path_local
+            "cd {path_local}/repo && git branch work && git checkout work"
         ))
         .await;
-        run_command(format!("cd {}/repo && git add .", path_local)).await;
         run_command(format!(
-            r#"cd {}/repo && git commit -m "hello""#,
-            path_local
+            "cd {path_local}/repo && echo 'hello' > hello.txt && git add . && git commit -m 'hello'"
         ))
         .await;
 
         // Push with the string which is unacceptable to the server hook.
-        let repo = RawRepositoryImpl::open(format!("{}/repo", path_local).as_str())
+        let repo = RawRepositoryImpl::open(format!("{path_local}/repo").as_str())
             .await
             .unwrap();
 
         let result = repo
             .push_option(
                 "origin".to_string(),
-                "test".to_string(),
+                "work".to_string(),
                 Some("reject".to_string()),
             )
             .await
@@ -247,7 +236,7 @@ echo "$result"
         assert!(!result);
 
         let result = repo
-            .push_option("origin".to_string(), "test".to_string(), None)
+            .push_option("origin".to_string(), "work".to_string(), None)
             .await
             .unwrap();
         assert!(!result);
@@ -256,12 +245,32 @@ echo "$result"
         let result = repo
             .push_option(
                 "origin".to_string(),
-                "test".to_string(),
+                "work".to_string(),
                 Some("accept".to_string()),
             )
             .await
             .unwrap();
         assert!(result);
+
+        // Push with acceptable string but with unacceptable branch.
+        run_command(format!(
+            "cd {path_local}/repo && git branch notwork && git checkout notwork"
+        ))
+        .await;
+        run_command(format!(
+            "cd {path_local}/repo && echo 'hello' > hello2.txt && git add . && git commit -m 'hello2'"
+        ))
+        .await;
+
+        let result = repo
+            .push_option(
+                "origin".to_string(),
+                "notwork".to_string(),
+                Some("accept".to_string()),
+            )
+            .await
+            .unwrap();
+        assert!(!result);
 
         server_task.await.unwrap();
     }
