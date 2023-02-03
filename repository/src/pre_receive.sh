a9e14224cffe80b6f1b74c49d1952360fa49ae75
JeongHunP <jhp0618@postech.ac.kr>
2023-02-03 16:18:46 +0900

    Modify pre_receive.sh

--- repository/src/pre_receive.sh
+++ repository/src/pre_receive.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 eval "count=\$GIT_PUSH_OPTION_COUNT"
-eval "simperby_path=\$SIMPERBY_PATH"
 eval "value=\$GIT_PUSH_OPTION_0"
+simperby_path=\$SIMPERBY_PATH
 
 read oldRev newRev refname
 branch="$(echo $refname | awk '{split($0,a,"/"); print a[3]}')"
@@ -12,7 +12,7 @@ then
 	exit 1
 fi
 
-$($simperby_path $value $branch)
+eval "$simperby_path $value $branch"
 status=$?
 if [ $status != 0 ]
 then exit 1
