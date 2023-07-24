b77c904edf1a8b5e1ff4dcc1183defaadfdadd39
Junha Yang <junhayang1@gmail.com>
2023-07-24 22:20:06 +0900

    Introduce NAT traversal module

---
+++ network/src/nat_traversal/mod.rs
@@ -0,0 +1,11 @@
+//! A module that provides NAT traversal for the node
+//! which enables the node to be able to become a server.
+//!
+//! TODO: based on the libp2p's NAT traversal implementation,
+//! describe the interface of this module so that it can be used with
+//! the DMS module.
+//!
+//! For example, it could be a magical function that returns a port that can be
+//! immediately bound to the socket.
+//! Or it could require an interactive process that asks the user to perform some
+//! complicated steps to configure the network.
